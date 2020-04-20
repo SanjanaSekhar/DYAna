9da72a0aa9e40b451ae0c1ca90c0d39322ea09aa
Oz Amram <oamram1@jhu.edu>
2020-04-20 13:39:56 -0500

    new root files

--- analyze/scripts/submit_everything.py
+++ analyze/scripts/submit_everything.py
@@ -10,13 +10,13 @@ def print_and_do(s):
 years = [2016, 2017,2018]
 prefixes = ["MuMu", "ElEl"]
 #scripts = ["MuMu_reco_background.C", "ElEl_reco_background.C"]
-scripts = ["ElEl_reco_mc.C", "MuMu_reco_mc.C"]
+scripts = ["MuMu_reco_mc.C", "ElEl_reco_mc.C"]
 njobs = 15
 
 #labels = ["wt", "ttbar", "diboson", "phot_ind"]
 #eos_files = ["WT_files.txt", "TTbar_files.txt", "diboson_files.txt", "PhotInd_files.txt"]
-#labels = ["dy"]
-#eos_files = ["DY_files.txt"]
+labels = ["dy"]
+eos_files = ["DY_files.txt"]
 ending = "april17"
 
 for i,script in enumerate(scripts):
