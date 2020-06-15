2bbcc953b1c016cd9214182c321c727477445f5a
Oz Amram <oz.amram@gmail.com>
2020-06-15 14:30:08 -0400

    Fix off by 1 bin bug in LQ reweights

--- plots/Gen_plots/LQ_rw_denom.C
+++ plots/Gen_plots/LQ_rw_denom.C
@@ -68,8 +68,8 @@ int make_amc_gen_cost(TTree *t_gen, TH2D *h_2d, float m_low, float m_high){
 }
 
 void normalize(TH2D *h){
-    for(int i=1; i<h->GetNbinsX(); i++){
-        for(int j=1; j<h->GetNbinsY(); j++){
+    for(int i=1; i<=h->GetNbinsX(); i++){
+        for(int j=1; j<=h->GetNbinsY(); j++){
             float xw = h->GetXaxis()->GetBinWidth(i);
             float yw = h->GetYaxis()->GetBinWidth(j);
             float content = h->GetBinContent(i,j);
@@ -89,8 +89,8 @@ void normalize(TH2D *h){
 void LQ_rw_denom(){
 
     bool write_out = true;
-    char *out_file = "../analyze/SFs/2018/LQ_rw.root";
-    TFile *f_gen = TFile::Open("../analyze/output_files/DY18_gen_level_april17.root");
+    char *out_file = "../analyze/SFs/2016/LQ_rw.root";
+    TFile *f_gen = TFile::Open("../analyze/output_files/DY16_gen_level_april17.root");
 
     TFile * f_out;
     if(write_out)
@@ -132,20 +132,30 @@ void LQ_rw_denom(){
     make_amc_gen_cost(t_gen_mu,  h_mu, m_low,m_high);
     make_amc_gen_cost(t_gen_el, h_el, m_low, m_high);
     
-    h_el->Print();
+    /*
+    TH1D *h_el_cost_pre = h_el->ProjectionY("h_el_cost_pre", 10,10);
+    TH1D *h_mu_cost_pre = h_mu->ProjectionY("h_mu_cost_pre", 10,10);
+    make_ratio_plot("LQ_cos_theta_pre.png", h_el_cost_pre, "El",h_mu_cost_pre, "Mu", "El/Mu", "cos(#theta_{*})", false, true);
+    */
 
     normalize(h_mu);
     normalize(h_el);
 
-    h_el->Print();
 
     /*
-    TCanvas *c_cost = new TCanvas("c_cost", "", 1000, 1000);
-    TH1D *h_cost = h_el->ProjectionY("h_cost");
-    h_cost->GetXaxis()->SetRangeUser(500., 1000.);
-    h_cost->Print("range");
-    
-    h_cost->Draw();
+    TH1D *h_el_cost = h_el->ProjectionY("h_el_cost", 10,10);
+    TH1D *h_mu_cost = h_mu->ProjectionY("h_mu_cost", 10,10);
+    make_ratio_plot("LQ_cos_theta.png", h_el_cost, "El",h_mu_cost, "Mu", "El/Mu", "cos(#theta_{*})", false, true);
+
+    TH1D *h_el_m = h_el->ProjectionX("h_el_m");
+    TH1D *h_mu_m = h_mu->ProjectionX("h_mu_m");
+    make_ratio_plot("LQ_m.png", h_el_m, "El",h_mu_m, "Mu", "El/Mu", "M (GeV)", false, true);
+
+
+    h_el_cost->SetLineColor(kBlue);
+    h_mu_cost->SetLineColor(kRed);
+    h_el_cost->Draw();
+    h_mu_cost->Draw("same");
     */
 
     if(write_out){
--- utils/PlotUtils.C
+++ utils/PlotUtils.C
@@ -78,7 +78,7 @@ TCanvas *draw_ratio_plot(string title, TH1F *h, TH1F *ratio, char axis_label[80]
 
 
 
-TCanvas* make_ratio_plot(string title, TH1F* h1, char h1_label[80], TH1F* h2, char h2_label[80], char ratio_label[80], 
+TCanvas* make_ratio_plot(string title, TH1* h1, char h1_label[80], TH1* h2, char h2_label[80], char ratio_label[80], 
         char axis_label[80], bool logy=false, bool write_out = true, float ratio_min = 0.5, float ratio_max = 1.5){
     //ratio is done as h1/h2
 
