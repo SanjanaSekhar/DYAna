92e7afd83d41199b0f02ff39ee221706868ccd4b
Sanjana Sekhar <ssekhar1@jhu.edu>
2020-06-15 18:21:16 -0400

    mu

--- plots/Fitting/LQ_draw_templates.C
+++ plots/Fitting/LQ_draw_templates.C
@@ -10,7 +10,7 @@ void LQ_draw_templates(){
         Double_t m_LQ=1000.;
         bool ss = false;
         bool use_xF =false;
-        bool use_LQ_denom=true;
+        bool use_LQ_denom=false;
         bool draw_muons = true;
         bool draw_electrons = false;
        const string sys_label = "";
