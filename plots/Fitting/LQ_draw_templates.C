310cb3d0f65f6bfe4029bd6cdc507dcaa8ee3bb8
Sanjana Sekhar <ssekhar1@jhu.edu>
2020-06-15 06:36:58 -0400

    using LQ denom

--- plots/Fitting/LQ_draw_templates.C
+++ plots/Fitting/LQ_draw_templates.C
@@ -14,7 +14,7 @@ void LQ_draw_templates(){
         bool draw_muons = false;
         bool draw_electrons = true;
        const string sys_label = "";
-        printf("=========================\n m_LQ = %f, use_LQ_denom = %d, draw_muons = %d, draw_electrons = %d \n=========================",m_LQ,use_LQ_denom,draw_muons,draw_electrons );
+        printf("=========================\n m_LQ = %f, use_LQ_denom = %d, draw_muons = %d, draw_electrons = %d \n=========================\n",m_LQ,use_LQ_denom,draw_muons,draw_electrons );
         int num_bins = n_lq_m_bins;
         const int n_rap_bins = 4;
         float rap_bins[] = {0., 0.6, 1., 1.5,  2.4};
@@ -63,14 +63,14 @@ void LQ_draw_templates(){
                     n_lq_m_bins, lq_m_bins, n_rap_bins, rap_bins, n_cost_bins, cost_bins);
             h_mumu_LQint->SetDirectory(0);
 
-            use_LQ_denom=true;
+            
 
             gen_mc_template(t_mumu_mc, h_mumu_sym, h_mumu_asym, h_mumu_alpha,h_mumu_LQpure, h_mumu_LQint, year, m_LQ, FLAG_MUONS, use_xF,use_LQ_denom, "");
 
-            sprintf(mu_fname1, "%s/Mu%i_MC_SM.png", plot_dir, year%2000);
+            sprintf(mu_fname1, "%s/Mu%i_MC%d_SM.png", plot_dir, year%2000,use_LQ_denom);
             //sprintf(mu_fname2, "%s/MuMu%i_M_fit_temps.png", plot_dir, year);
-            sprintf(mu_fname2, "%s/Mu%i_MC_LQint.png", plot_dir, year%2000);
-            sprintf(mu_fname3, "%s/Mu%i_MC_LQpure.png", plot_dir, year%2000);
+            sprintf(mu_fname2, "%s/Mu%i_MC%d_LQint.png", plot_dir, year%2000, use_LQ_denom);
+            sprintf(mu_fname3, "%s/Mu%i_MC%d_LQpure.png", plot_dir, year%2000, use_LQ_denom);
 
             auto h_mumu_pl = *h_mumu_sym + *h_mumu_asym;
             auto h_mumu_mn = *h_mumu_sym - *h_mumu_asym;
@@ -193,10 +193,10 @@ void LQ_draw_templates(){
 
             gen_mc_template(t_elel_mc, h_elel_sym, h_elel_asym, h_elel_alpha,h_elel_LQpure, h_elel_LQint, year, m_LQ, FLAG_ELECTRONS, use_xF,use_LQ_denom, "");
 
-            sprintf(el_fname1, "%s/El%i_MC_SM.png", plot_dir, year%2000);
+            sprintf(el_fname1, "%s/El%i_MC%d_SM.png", plot_dir, year%2000,use_LQ_denom);
             //sprintf(el_fname2, "%s/ElEl%i_M_fit_temps.png", plot_dir, year);
-            sprintf(el_fname2, "%s/El%i_MC_LQint.png", plot_dir, year%2000);
-            sprintf(el_fname3, "%s/El%i_MC_LQpure.png", plot_dir, year%2000);
+            sprintf(el_fname2, "%s/El%i_MC%d_LQint.png", plot_dir, year%2000,use_LQ_denom);
+            sprintf(el_fname3, "%s/El%i_MC%d_LQpure.png", plot_dir, year%2000,use_LQ_denom);
 
             auto h_elel_pl = *h_elel_sym + *h_elel_asym;
             auto h_elel_mn = *h_elel_sym - *h_elel_asym;
