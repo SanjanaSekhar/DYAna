dfe286946331707a210386641215ce3984fe3942
Sanjana Sekhar <ssekhar1@jhu.edu>
2020-06-23 01:37:16 -0400

    Update LQ_TemplateMaker_systematics.C

--- utils/LQ_TemplateMaker_systematics.C
+++ utils/LQ_TemplateMaker_systematics.C
@@ -252,8 +252,8 @@ int gen_mc_template(TTree *t1, TH3F* h_sym, TH3F *h_asym, TH3F *h_alpha, TH3F *h
            if(use_LQ_denom) reweight_LQpure_pos = (reweight_LQpure_norm*reweight_LQpure_num/LQ_denom);
            else reweight_LQpure_pos = (reweight_LQpure_norm*reweight_LQpure_num/denom);
            if(reweight_LQpure_pos>50.)reweight_LQpure_pos = 0.;
-           reweight_LQpure_num1 = ((1 - gen_cost)*(1 - gen_cost));
-            reweight_LQpure_denom1 = (((2*m_LQ*m_LQ/s)+1-gen_cost)* ((2*m_LQ*m_LQ/s)+1-gen_cost));
+           reweight_LQpure_num1 = ((1 + gen_cost)*(1 + gen_cost));
+            reweight_LQpure_denom1 = (((2*m_LQ*m_LQ/s)+1+gen_cost)* ((2*m_LQ*m_LQ/s)+1+gen_cost));
             reweight_LQpure_num =(reweight_LQpure_num1/reweight_LQpure_denom1);
             Double_t reweight_LQpure_neg;
            if(use_LQ_denom) reweight_LQpure_neg = (reweight_LQpure_norm*reweight_LQpure_num/LQ_denom);
