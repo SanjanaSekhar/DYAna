427d99b17436072a3dd52f93fb8bc16702274e4e
Sanjana Sekhar <ssekhar1@jhu.edu>
2020-07-07 09:19:50 -0400

    Update LQ_draw_templates.C

--- plots/Fitting/LQ_draw_templates.C
+++ plots/Fitting/LQ_draw_templates.C
@@ -184,16 +184,17 @@ void LQ_draw_templates(){
             //h1_mumu_LQpure->SetMaximum(h1_mumu_LQint->GetMaximum()*1.2);
             
             TCanvas *c_mumu1 = new TCanvas("c_mumu", "Histograms", 200, 10, 900, 700);
-            h1_mumu_asym->SetTitle(mu_title); 
-            h1_mumu_asym->Draw("hist");
-            h1_mumu_sym->Draw("hist same ");
+            h1_mumu_alpha->SetTitle(mu_title); 
+            //h1_mumu_asym->Draw("hist");
+            //h1_mumu_sym->Draw("hist same ");
+            h1_mumu_alpha->Draw("hist");
             h1_mumu_pl->Draw("hist same");
             h1_mumu_mn->Draw("hist same");
-            h1_mumu_alpha->Draw("hist same");
+            
 
             TLegend *leg1 = new TLegend(x_start, y_start, x_end, y_end);
-            leg1->AddEntry(h1_mumu_asym, "Asym Template", "l");
-            leg1->AddEntry(h1_mumu_sym, "Sym Template", "l");
+            //leg1->AddEntry(h1_mumu_asym, "Asym Template", "l");
+            //leg1->AddEntry(h1_mumu_sym, "Sym Template", "l");
             leg1->AddEntry(h1_mumu_pl, "Plus Template", "l");
             leg1->AddEntry(h1_mumu_mn, "Minus Template", "l");
             leg1->AddEntry(h1_mumu_alpha, "alpha Template", "l");
@@ -302,15 +303,7 @@ void LQ_draw_templates(){
             h1_elel_LQint_u->SetLineWidth(2);
             h1_elel_LQpure_d->SetLineWidth(2);
             h1_elel_LQint_d->SetLineWidth(2);
-/*
-            printf("\nyear = %i\n",year );
-            for(int i=1;i<=h_elel_LQpure_u->GetNbinsX();i++)
-            {for(int j=1;j<=h_elel_LQpure_u->GetNbinsY();j++)
-                {for(int k=1;k<=h_elel_LQpure_u->GetNbinsZ();k++)
-                    printf("m_bin = %i, rap_bin = %i, cost_bin = %i, el_content = %f\n",i,j,k,h_elel_LQpure_u->GetBinContent(i,j,k) );
-                }
-            }
-*/
+
             if(year==2016)
             {
                 h16_elel_LQpure_u = h1_elel_LQpure_u;
@@ -336,12 +329,13 @@ void LQ_draw_templates(){
             //h1_elel_LQpure->SetMaxielm(h1_elel_LQint->GetMaxielm()*1.2);
             
             TCanvas *c_elel1 = new TCanvas("c_elel", "Histograms", 200, 10, 900, 700);
-            h1_elel_asym->SetTitle(el_title); 
-            h1_elel_asym->Draw("hist");
-            h1_elel_sym->Draw("hist same ");
+            h1_elel_alpha->SetTitle(el_title); 
+            //h1_elel_asym->Draw("hist");
+            //h1_elel_sym->Draw("hist same ");
+            h1_elel_alpha->Draw("hist");
             h1_elel_pl->Draw("hist same");
             h1_elel_mn->Draw("hist same");
-            h1_elel_alpha->Draw("hist same");
+            
 
             TLegend *leg1 = new TLegend(x_start, y_start, x_end, y_end);
             leg1->AddEntry(h1_elel_asym, "Asym Template", "l");
