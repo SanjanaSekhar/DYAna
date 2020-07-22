051a219a6da36249b59f61227c05b605eba78d37
Sanjana <sanjanasekhar39@gmail.com>
2020-07-22 13:39:29 -0500

    e

--- analyze/combine/AFB_fits/scripts/LQ_do_fits.py
+++ analyze/combine/AFB_fits/scripts/LQ_do_fits.py
@@ -22,8 +22,8 @@ for y in [-1]:
     extra_params=""
     bin_start = 0
     bin_stop = 8
-    options.chan="ee"
-    options.q="d"
+    options.chan="mumu"
+    options.q="u"
     options.no_sys=False
     options.fake_data=True
     options.year = y
