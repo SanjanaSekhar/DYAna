374f5bc4f52ae71518690ef1ba62af35fc0f2dd7
Sanjana Sekhar <ssekhar1@jhu.edu>
2020-07-25 21:59:12 -0400

    Update LQ_draw_templates.C

--- plots/Fitting/LQ_draw_templates.C
+++ plots/Fitting/LQ_draw_templates.C
@@ -117,9 +117,9 @@ void LQ_draw_templates(){
             gen_mc_template(t_mumu_mc, h_mumu_sym, h_mumu_asym, h_mumu_alpha,h_mumu_LQpure_u, h_mumu_LQint_u,h_mumu_LQpure_d, h_mumu_LQint_d, 
                 year, m_LQ, FLAG_MUONS, use_xF,"");
 
-            sprintf(mu_fname1, "%s/Mu%i_MC_SMudm_m%i.png", plot_dir, year%2000,int(m_LQ));
-            sprintf(mu_fname2, "%s/Mu%i_MCudm_LQpure_m%i.png", plot_dir, year%2000, int(m_LQ));
-            sprintf(mu_fname3, "%s/Mu%i_MCudm_LQint_m%i.png", plot_dir, year%2000, int(m_LQ));
+            sprintf(mu_fname1, "%s/Mu%i_MC6cost_SM_m%i.png", plot_dir, year%2000,int(m_LQ));
+            sprintf(mu_fname2, "%s/Mu%i_MC6cost_LQpure_m%i.png", plot_dir, year%2000, int(m_LQ));
+            sprintf(mu_fname3, "%s/Mu%i_MC6cost_LQint_m%i.png", plot_dir, year%2000, int(m_LQ));
 
             auto h_mumu_pl = *h_mumu_sym + *h_mumu_asym;
             auto h_mumu_mn = *h_mumu_sym - *h_mumu_asym;
@@ -215,7 +215,7 @@ void LQ_draw_templates(){
             leg2->AddEntry(h1_mumu_LQpure_d,"d-LQpure Template","l");
             leg2->Draw();
 
-          //  c_mumu2->Print(mu_fname2);
+            c_mumu2->Print(mu_fname2);
             delete c_mumu2;
 
             TCanvas *c_mumu3 = new TCanvas("c_mumu3", "Histograms", 200, 10, 900, 700);
@@ -228,7 +228,7 @@ void LQ_draw_templates(){
             leg3->AddEntry(h1_mumu_LQint_d,"d-LQint Template","l");
             leg3->Draw();
             
-          // c_mumu3->Print(mu_fname3);
+           c_mumu3->Print(mu_fname3);
             delete c_mumu3;
 
         }
@@ -267,9 +267,9 @@ void LQ_draw_templates(){
             gen_mc_template(t_elel_mc, h_elel_sym, h_elel_asym, h_elel_alpha,h_elel_LQpure_u, h_elel_LQint_u,h_elel_LQpure_d, h_elel_LQint_d, 
                 year, m_LQ, FLAG_ELECTRONS, use_xF, "");
 
-            sprintf(el_fname1, "%s/El%i_MC_SMudm_m%i.png", plot_dir, year%2000,int(m_LQ));
-            sprintf(el_fname2, "%s/El%i_MCudm_LQpure_m%i.png", plot_dir, year%2000,int(m_LQ));
-            sprintf(el_fname3, "%s/El%i_MCudm_LQint_m%i.png", plot_dir, year%2000,int(m_LQ));
+            sprintf(el_fname1, "%s/El%i_MC6cost_SM_m%i.png", plot_dir, year%2000,int(m_LQ));
+            sprintf(el_fname2, "%s/El%i_MC6cost_LQpure_m%i.png", plot_dir, year%2000,int(m_LQ));
+            sprintf(el_fname3, "%s/El%i_MC6cost_LQint_m%i.png", plot_dir, year%2000,int(m_LQ));
 
             auto h_elel_pl = *h_elel_sym + *h_elel_asym;
             auto h_elel_mn = *h_elel_sym - *h_elel_asym;
@@ -363,7 +363,7 @@ void LQ_draw_templates(){
             leg2->AddEntry(h1_elel_LQpure_d,"d-LQpure Template","l");
             leg2->Draw();
 
-         //   c_elel2->Print(el_fname2);
+            c_elel2->Print(el_fname2);
             delete c_elel2;
 
             TCanvas *c_elel3 = new TCanvas("c_elel3", "Histograms", 200, 10, 900, 700);
@@ -376,7 +376,7 @@ void LQ_draw_templates(){
             leg3->AddEntry(h1_elel_LQint_d,"d-LQint Template","l");
             leg3->Draw();
             
-         //   c_elel3->Print(el_fname3);
+            c_elel3->Print(el_fname3);
             delete c_elel3;
          
         }
