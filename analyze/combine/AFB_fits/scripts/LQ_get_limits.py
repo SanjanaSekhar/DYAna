0abbbebccfdb39211829f1d718b0c2228b437b50
Sanjana Sekhar <ssekhar1@jhu.edu>
2020-07-28 03:07:48 -0400

    limits

---
+++ analyze/combine/AFB_fits/scripts/LQ_get_limits.py
@@ -0,0 +1,78 @@
+
+from LQ_utils_limits import *
+import ROOT
+from ROOT import *
+
+parser = OptionParser(usage="usage: %prog [options] in.root  \nrun with --help to get list of options")
+parser.add_option("--chan",  default="combined", type="string", help="What channels to run the fit over (combined, ee, or mumu)")
+parser.add_option("--q",  default="combined", type="string", help="What channels to run the fit over (combined, u, or d)")
+parser.add_option("--no_plot",  default=False, action="store_true", help="Don't make postfit plots")
+parser.add_option("--no_sys",  default=False, action="store_true", help="Use fit template without any shape systematics")
+parser.add_option("--fake_data",  default=False, action="store_true", help="Use fit template without any shape systematics and no fakes")
+parser.add_option("--no_cleanup",  default=False, action="store_true", help="Don't remove root files created by fit")
+parser.add_option("--mbin", default = -1, type='int', help="Only do fits for this single mass bin, default is all bins")
+parser.add_option("-y", "--year", default = -1, type='int', help="Only do fits for this single year (2016,2017, or 2018), default is all years")
+
+(options, args) = parser.parse_args()
+
+
+
+for y in [-1]:
+
+    extra_params=""
+    #channels=['eu','ed','mu','md']
+   # options.q="u"
+    options.no_sys=False
+    options.fake_data=True
+    options.year = y
+    '''
+    if(options.chan == "ee"):
+        print("Chan is ee, will mask mumu channels")
+        if(options.year < 0): 
+            extra_params += " --setParameters mask_Y16_mumu16=1,mask_Y17_mumu17=1,mask_Y18_mumu18=1" 
+        else:
+            extra_params += " --setParameters mask_Y%i_mumu%i=1" % (options.year % 2000, options.year % 2000)
+    elif(options.chan == "mumu"):
+        print("Chan is mumu, will mask ee and ee_ss channels")
+        if(options.year < 0):
+            extra_params += " --setParameters mask_Y16_ee16=1,mask_Y17_ee17=1,mask_Y18_ee18=1,mask_Y16_ee16_ss=1,mask_Y17_ee17_ss=1,mask_Y18_ee18_ss=1" 
+        else:
+            extra_params += " --setParameters mask_Y%i_ee%i=1,mask_Y%i_ee%i_ss=1" % (options.year % 2000, options.year % 2000, options.year % 2000, options.year % 2000)
+    '''
+    
+    fit_name = options.chan
+    if(options.no_sys): fit_name +="_nosys"
+    if(options.fake_data): fit_name +="_fake_data"
+    if(options.year > 0): fit_name +="_y%i" % (options.year % 2000)
+    fit_name+="_"+options.q
+    print("\n fit_name = ", fit_name)
+
+    workspace="workspaces/%s_LQ.root" % (options.chan)
+    make_workspace(workspace, options.no_sys, options.fake_data, year = options.year)
+    '''
+        plotdir="postfit_plots/%s_LQ_m%i" % (fit_name,mLQ)
+        print("\n plotdir = ", plotdir)
+        print_and_do("[ -e %s ] && rm -r %s" % (plotdir, plotdir))
+        print_and_do("mkdir %s" % (plotdir))
+        #print_and_do("combine %s -M MultiDimFit  --saveWorkspace --saveFitResult --robustFit 1 %s --setParameters A0=0.05 --freezeParameters A0 -v 2" %(workspace, extra_params))
+        print_and_do("combine %s -M MultiDimFit  --saveWorkspace --saveFitResult --robustFit 1 %s " %(workspace, extra_params))
+
+        if(not options.no_plot):
+            print_and_do("PostFitShapesFromWorkspace -w higgsCombineTest.MultiDimFit.mH120.root -f multidimfit.root:fit_mdf --postfit -o %s_fit_shapes_LQ.root --sampling --samples 100"
+                    % (fit_name))
+            extra_args = ""
+            if(options.year > 0): extra_args = " -y %i " % options.year
+            print_and_do("python scripts/LQ_plot_postfit.py -i %s_fit_shapes_LQ.root -o %s  %s --mLQ %i --chan %s --q %s" % (fit_name, plotdir, extra_args,mLQ,options.chan,options.q))
+            print_and_do("combine %s -M FitDiagnostics --skipBOnlyFit %s" % (workspace, extra_params)) #only to get prefit, probably a better way
+            print_and_do("python scripts/my_diffNuisances.py multidimfit.root --multidim --mLQ %i --prefit fitDiagnostics.root -p Afb --skipFitB -g %s" % (mLQ, plotdir))
+            print_and_do("mv %s_fit_shapes_LQ.root %s" %(fit_name, plotdir))
+            if(not options.no_cleanup): print_and_do("rm fitDiagnostics.root higgsCombineTest.FitDiagnostics.mH120.root")
+
+
+        print_and_do("""echo "fit_mdf->Print();" > cmd.txt""")
+        print_and_do("""echo ".q" >> cmd.txt """)
+        print_and_do("root -l -b multidimfit.root < cmd.txt > fit_results/%s_m%i.txt" % (fit_name,mLQ))
+        print_and_do("rm -f cards/sed*")
+        if(not options.no_cleanup): print_and_do("rm cmd.txt combine_logger.out higgsCombineTest.MultiDimFit.mH120.root multidimfit.root")
+
+'''
--- analyze/combine/LQ_merge_workspaces.C
+++ analyze/combine/LQ_merge_workspaces.C
@@ -29,57 +29,66 @@ void CopyDir(TDirectory *source, TDirectory *savdir) {
 
 void LQ_merge_workspaces(){
 
+    for(int i=1.5;i<=3.5;i+=0.5){
+        int mLQ = 1000*i;
+        for(int year=16;year<=18;year++){
 
-    const TString f1_s("combine/templates/LQm1000_6cost_templates16.root");
-    const TString fout_s("combine/templates/LQm1000_6cost_merge_templates16.root");
-    TFile *f1 = TFile::Open(f1_s, "READ");
-    TFile *fout = TFile::Open(fout_s, "RECREATE");
-    char dirname[40];
-
-
-    char *sys_base = "root://cmseos.fnal.gov//store/user/sasekhar/Condor_outputs/templ16_sys_jul25LQ";
-    char *pdf_base = "root://cmseos.fnal.gov//store/user/sasekhar/Condor_outputs/templ16_pdf_jul25LQ";
-    int num_sys_files = 20;
-    int num_pdf_files = 20;
-
-    std::vector<string> fs;
-    for(int i=0; i<num_sys_files; i++){
-        char fname[180];
-        sprintf(fname, "%s/file_%i.root", sys_base, i);
-        fs.push_back(string(fname));
-    }
-    for(int i=0; i<num_pdf_files; i++){
-        char fname[180];
-        sprintf(fname, "%s/file_%i.root", pdf_base, i);
-        fs.push_back(string(fname));
-    }
-   
-    int i_start=0;
-    int i_max = 8;
+            char *ending="jul27LQ"
+            char f1_s[180];
+            sprintf(f1_s,"root://cmseos.fnal.gov//store/user/sasekhar/Condor_outputs/templ%i_nonsys_%s_m%i/file_0.root", year, ending, mLQ)
+            char fout_s[180];
+            sprintf(fout_s,"combine/mtemps/LQm%i_merge_templates%i.root", mLQ, year)
+            //const TString f1_s("root://cmseos.fnal.gov//store/user/sasekhar/Condor_outputs/templ16_nonsys_jul27LQ");
+            //const TString fout_s("combine/templates/LQm%i_merge_templates%i.root");
+            TFile *f1 = TFile::Open(f1_s, "READ");
+            TFile *fout = TFile::Open(fout_s, "RECREATE");
+            char dirname[40];
 
 
+            char sys_base[180]
+            sprintf(sys_base,"root://cmseos.fnal.gov//store/user/sasekhar/Condor_outputs/templ%i_sys_%s_m%i", year, ending, mLQ);
+            char pdf_base[180]
+            sprintf(pdf_base,"root://cmseos.fnal.gov//store/user/sasekhar/Condor_outputs/templ%i_pdf_%s_m%i", year, ending, mLQ);
+            
+            
+            int num_sys_files = 20;
+            int num_pdf_files = 20;
 
-        snprintf(dirname, 10, "LQ");
-        fout->mkdir(dirname);
-        fout->cd(dirname);
-        TDirectory *savedir = gDirectory;
+            std::vector<string> fs;
+            for(int i=0; i<num_sys_files; i++){
+                char fname[180];
+                sprintf(fname, "%s/file_%i.root", sys_base, i);
+                fs.push_back(string(fname));
+            }
+            for(int i=0; i<num_pdf_files; i++){
+                char fname[180];
+                sprintf(fname, "%s/file_%i.root", pdf_base, i);
+                fs.push_back(string(fname));
+            }
 
-        f1->cd(dirname);
-        CopyDir(gDirectory, savedir);
-        savedir->cd();
-        for(auto f_name = fs.begin(); f_name !=fs.end(); f_name++){
-            TFile *f2 = TFile::Open(f_name->c_str(), "READ");
-            printf("Opening file: %s \n\n\n", f_name->c_str());
-            f2->cd();
-            f2->cd(dirname);
-            TDirectory *source_dir = gDirectory;
-            CopyDir(source_dir, savedir);
-            f2->Close();
+                snprintf(dirname, 10, "LQ");
+                fout->mkdir(dirname);
+                fout->cd(dirname);
+                TDirectory *savedir = gDirectory;
+
+                f1->cd(dirname);
+                CopyDir(gDirectory, savedir);
+                savedir->cd();
+                for(auto f_name = fs.begin(); f_name !=fs.end(); f_name++){
+                    TFile *f2 = TFile::Open(f_name->c_str(), "READ");
+                    printf("Opening file: %s \n\n\n", f_name->c_str());
+                    f2->cd();
+                    f2->cd(dirname);
+                    TDirectory *source_dir = gDirectory;
+                    CopyDir(source_dir, savedir);
+                    f2->Close();
+                }
+
+            
+            fout->Print();
+            fout->Write();
+            fout->Close();
         }
-
-    
-    fout->Print();
-    fout->Write();
-    fout->Close();
+    }
 }
 
