d3f17dad9c69d6198baa0f5c162efb1c92f0b5de
Sanjana Sekhar <ssekhar1@jhu.edu>
2020-07-29 05:52:32 -0400

    Update LQ_get_limits.py

--- analyze/combine/AFB_fits/scripts/LQ_get_limits.py
+++ analyze/combine/AFB_fits/scripts/LQ_get_limits.py
@@ -28,7 +28,7 @@ def plotLimits(channel):
      
      # Create an empty TH1 from the first TGraph to serve as the pad axis and frame
     axis = CreateAxisHist(graphs.values()[0])
-    axis.GetXaxis().SetTitle('m_{%s} (GeV)'%(channel))
+    axis.GetXaxis().SetTitle('m_{S_{%s}} (GeV)'%(channel))
     axis.GetYaxis().SetTitle('95%% CL limit on y_{%s}'%(channel))
     pads[0].cd()
     axis.Draw('axis')
@@ -107,12 +107,12 @@ for channel in ['eu','ed','mu','md']:
         
         print("\n=========completed card for channel %s mass %i =========\n"%(channel,mass))
         print("\n========= making workspace for %s mass %i =========\n"%(channel,mass))
-        print_and_do("text2workspace.py %s -P LQ_Analysis.DYAna.LQ_my_model:dy_AFB -o %s --channel-masks" % (comb_card, workspace))
+     #   print_and_do("text2workspace.py %s -P LQ_Analysis.DYAna.LQ_my_model:dy_AFB -o %s --channel-masks" % (comb_card, workspace))
         print("\n========= extracting upper limits for %s mass %i =========\n"%(channel, mass))
-        print_and_do("combineTool.py -d %s -M AsymptoticLimits -m %i -n .limit --there"%(workspace,mass))
+    #    print_and_do("combineTool.py -d %s -M AsymptoticLimits -m %i -n .limit --there"%(workspace,mass))
 
     print("\n========= collecting limits for channel %s and making json =========\n"%(channel))
-    print_and_do("combineTool.py -M CollectLimits LQ_cards/%s/*/*limit* --use-dirs -o LQ_cards/%s/limits.json"%(channel,channel))
+   # print_and_do("combineTool.py -M CollectLimits LQ_cards/%s/*/*limit* --use-dirs -o LQ_cards/%s/limits.json"%(channel,channel))
 
     print("\n========= making limit plot for channel %s =========\n"%(channel))
     plotLimits(channel)
