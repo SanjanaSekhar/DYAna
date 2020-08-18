cc7b6645830cdad03b3c1c30545d37a22b1115e3
Sanjana Sekhar <ssekhar1@jhu.edu>
2020-08-18 18:13:20 -0400

    Update LQ_my_model.py

--- analyze/combine/LQ_my_model.py
+++ analyze/combine/LQ_my_model.py
@@ -10,7 +10,7 @@ class DY_AFB(PhysicsModel):
         """Create POI and other parameters, and define the POI set."""
 
         self.modelBuilder.doVar("Afb[0.6, -1.0,1.0]");
-        self.modelBuilder.doVar("A0[0.05, -1.0, 1.0]");
+        self.modelBuilder.doVar("A0[0.05, 0.001, 1.0]");
         self.modelBuilder.doVar("yLQ[0.01, -5.0, 5.0]");
         self.modelBuilder.doSet("POI","yLQ")
 
