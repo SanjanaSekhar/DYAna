43411c7ad2ad4b45e9fb442ba3245d0d2a850752
Sanjana Sekhar <ssekhar1@jhu.edu>
2020-08-20 02:16:24 -0400

    Update LQ_do_fits.py

--- analyze/combine/AFB_fits/scripts/LQ_do_fits.py
+++ analyze/combine/AFB_fits/scripts/LQ_do_fits.py
@@ -22,8 +22,8 @@ for y in [-1]:
     extra_params=""
     options.chan="ee"
     options.q="d"
-    options.no_sys=False
-    options.fake_data=True
+    options.no_sys=True
+    options.fake_data=False
     options.year = y
     '''
     if(options.chan == "ee"):
