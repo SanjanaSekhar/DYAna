c77cd5092fb5fd63c5382c497c06e0dac995143d
Sanjana Sekhar <ssekhar1@jhu.edu>
2020-09-21 20:00:39 -0400

    conschk

--- utils/LQ_TemplateMaker_systematics.C
+++ utils/LQ_TemplateMaker_systematics.C
@@ -208,7 +208,7 @@ int gen_mc_template(TTree *t1, TH3F* h_sym, TH3F *h_asym, TH3F *h_alpha, TH3F *h
     for (int i=0; i<tm.nEntries; i++) {
         tm.getEvent(i);
         bool pass = (tm.m >= lq_m_bins[0]) && (tm.met_pt < met_cut)  && tm.has_no_bjets && tm.not_cosmic;
-        if(pass){
+        if(pass && tm.evt_pdfweight>0.){
 
             tm.doCorrections();
             tm.getEvtWeight();
