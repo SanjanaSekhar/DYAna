8e804f0acfd66b523867c8ff06a2f8b2dac1ef25
Oz Amram <oz.amram@gmail.com>
2021-04-06 17:26:25 -0400

    Small plotting changes

--- plots/Gen_plots/draw_generator_cmp.C
+++ plots/Gen_plots/draw_generator_cmp.C
@@ -54,6 +54,8 @@ void draw_generator_cmp(){
     sprintf(plt_title3, "pwg_vs_mad_m%.0f_cost_xf_cmp.pdf", m_low);
     sprintf(plt_title4, "pwg_vs_mad_m%.0f_cost_r_cmp.pdf", m_low);
 
+    TH1F *h_mad_m = new TH1F("h_mad_m", "", 30, 150,1000);
+    TH1F *h_pwg_m = new TH1F("h_pwg_m", "", 30, 150,1000);
 
     TH1F *h_pwg_cost_st = new TH1F("h_pwg_cost_st", title, 20, -1., 1.);
     TH1F *h_pwg_cost_r = new TH1F("h_pwg_cost_r", title, 20, -1., 1.);
@@ -66,8 +68,8 @@ void draw_generator_cmp(){
     TH1F *h_mad_xf = new TH1F("h_mad_xf", title, 20, 0.,.5);
 
 
-    make_amc_gen_cost(t_mad_mu,  h_mad_cost_st, h_mad_cost_r, h_mad_pt, h_mad_xf, m_low, m_high, pt_low, pt_high, rap_low, rap_high);
-    make_amc_gen_cost(t_mad_el,  h_mad_cost_st, h_mad_cost_r, h_mad_pt, h_mad_xf, m_low, m_high, pt_low, pt_high, rap_low, rap_high);
+    make_amc_gen_cost(t_mad_mu,  h_mad_m, h_mad_cost_st, h_mad_cost_r, h_mad_pt, h_mad_xf, m_low, m_high, pt_low, pt_high, rap_low, rap_high);
+    make_amc_gen_cost(t_mad_el,  h_mad_m, h_mad_cost_st, h_mad_cost_r, h_mad_pt, h_mad_xf, m_low, m_high, pt_low, pt_high, rap_low, rap_high);
     make_gen_cost(t_pwg,  h_pwg_cost_st, h_pwg_cost_r, h_pwg_pt, h_pwg_xf, m_low, m_high, phot_ind);
 
     h_mad_cost_st->Scale(1./h_mad_cost_st->Integral());
--- plots/Gen_plots/draw_phot_pdf_cmp.C
+++ plots/Gen_plots/draw_phot_pdf_cmp.C
@@ -9,9 +9,12 @@ void draw_phot_pdf_cmp(){
     //TFile * f_mad = TFile::Open("..//generator_stuff/root_files/phot_induced_aug21.root");
     TTree *t_mad = (TTree *)f_mad->Get("T_lhe");
 
-    //TFile *f_pwg = TFile::Open("../generator_stuff/root_files/photInd_lux_may2.root");
-    TFile *f_pwg = TFile::Open("../generator_stuff/root_files/PhotInd_M150_mar29.root");
+    TFile *f_pwg = TFile::Open("../generator_stuff/root_files/photInd_lux_may2.root");
+    //TFile *f_pwg = TFile::Open("../generator_stuff/root_files/PhotInd_M150_mar29.root");
     TTree *t_pwg = (TTree *) f_pwg->Get("T_lhe");
+
+    TH1F *h_mad_m = new TH1F("h_mad_m", "", 30, 150,1000);
+    TH1F *h_pwg_m = new TH1F("h_pwg_m", "", 30, 150,1000);
 
     char title[80] = "LO vs. NLO QED, Lux (M > 150)";
     TH1F *h_pwg_cost_st = new TH1F("h_pwg_cost_st", title, 20, -1., 1.);
@@ -30,7 +33,7 @@ void draw_phot_pdf_cmp(){
     float pt_high = 100000.;
     bool phot_ind = true;
 
-    make_gen_cost(t_mad,  h_mad_cost_st, h_mad_cost_r, h_mad_pt, h_mad_xf, m_low, m_high,  pt_low,  pt_high, phot_ind);
+    make_gen_cost(t_mad,   h_mad_cost_st, h_mad_cost_r, h_mad_pt, h_mad_xf, m_low, m_high,  pt_low,  pt_high, phot_ind);
     make_gen_cost(t_pwg,  h_pwg_cost_st, h_pwg_cost_r, h_pwg_pt, h_pwg_xf, m_low, m_high,  pt_low,  pt_high, phot_ind);
 
     h_mad_cost_st->Scale(1./h_mad_cost_st->Integral());
