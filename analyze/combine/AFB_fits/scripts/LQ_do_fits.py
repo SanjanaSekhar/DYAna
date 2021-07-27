8ba7337ed4d4ada6cbed98707ff4f913ebe09799
Sanjana <sanjanasekhar39@gmail.com>
2021-07-27 10:38:07 -0500

    mu 3rap corrected

--- analyze/combine/AFB_fits/scripts/LQ_do_fits.py
+++ analyze/combine/AFB_fits/scripts/LQ_do_fits.py
@@ -22,8 +22,8 @@ parser.add_option("--noSymMCStats", default = False, action="store_true",  help=
 for y in [-1]:
 
     extra_params=""
-    options.chan="ee"
-    options.q="d"
+    options.chan="mumu"
+    options.q="u"
     options.no_sys=False
     options.fake_data=True
     options.year = y
