969718827d5c2d3c7de033bc2cc656cbeb518e83
Sanjana Sekhar <ssekhar1@jhu.edu>
2022-06-07 11:52:56 -0400

    all chan and q

--- analyze/combine/AFB_fits/scripts/LQ_check_sys_uncs.py
+++ analyze/combine/AFB_fits/scripts/LQ_check_sys_uncs.py
@@ -17,151 +17,151 @@ parser.add_option("--diff", default=False, action="store_true", help="Diff")
 
 (options, args) = parser.parse_args()
 
-chan = "mumu"
-q = "u"
-options.mLQ = 1000
-fake_data = True
-no_sys = False
-gen_level = False
-no_LQ = False
-year = -1
-#extra_params = "--X-rtd MINIMIZER_no_analytic"
+for chan in ["mumu","ee"]:
+    for q in ["u","d"]:
+        options.mLQ = 1000
+        fake_data = True
+        no_sys = False
+        gen_level = False
+        no_LQ = False
+        year = -1
+        #extra_params = "--X-rtd MINIMIZER_no_analytic"
 
-s = 123456
-extra_params = " -s %i" % s
+        s = 123456
+        extra_params = " -s %i" % s
 
-if chan == "ee":
-    individual_pars = [ "dy_xsec", "db_xsec",  "top_xsec", "gam_xsec",  "elFakesYR",  "Pu", "prefireYR"]
-    group_pars =[  "RFscalesYRC", "emucostrwsYRC", "ptrwsYRC", "pdfs", "lumisYR","elScalesYR", "elHLTsYR", "elIDs", "elRECOs",  
-                    "elfakesrwsYR", "autoMCStats"] 
-    #"BTAGSYR","muPrefYRC","METJECYR",
-else:
-    individual_pars = [ "dy_xsec", "db_xsec",  "top_xsec", "gam_xsec",  "muFakesYR", "Pu", "prefireYR",  "muRCYR", ]
-    group_pars =[  "RFscalesYRC", "emucostrwsYRC", "ptrwsYRC", "pdfs", "lumisYR","muIDsYR", "muHLTsYR", 
-                    "mufakesrwsYR",  "autoMCStats"] 
-
-sys_name_conv = dict()
-
-sys_name_conv['dy_xsec'] = "DY Cross Section"
-sys_name_conv['db_xsec'] = "Diboson Cross Section"
-sys_name_conv['top_xsec'] = "$\\ttbar$ Cross Section"
-sys_name_conv['gam_xsec'] = "$\\PGg\\PGg$ Cross Section"
-sys_name_conv['RFscalesYRC'] = "$\\alpha_s$ + Renormalization/Factorization Scales"
-sys_name_conv['emucostrwsYRC'] = "$e\\mu$ Shape Corrections"
-sys_name_conv['ptrwsYRC'] = "DY $p_{T}$ Correction"
-sys_name_conv['pdfs'] = "PDFs"
-sys_name_conv['lumisYR'] = "Luminosity"
-sys_name_conv['autoMCStats'] = "MC and MisID Backgrounds Statistical Uncertainty"
-sys_name_conv['Pu'] = "Pileup"
-
-if chan == "ee": 
-    sys_name_conv['elFakesYR'] = "Electron MisID Normalization"
-    sys_name_conv['elScalesYR'] = "Electron Momentum Scale"
-    sys_name_conv['elHLTsYR'] = "Electron Trigger"
-    sys_name_conv['elIDs'] = "Electron Identification/Isolation"
-    sys_name_conv['elRECOs'] = "Electron Reconstruction"
-    sys_name_conv['elfakesrwsYR'] = "Electron MisID Shape"
-    sys_name_conv['prefireYR'] = "Electron Trigger Prefire Correction"
-
-if chan== "mumu": 
-    sys_name_conv['muFakesYR'] = "Muon MisID Normalization"
-    sys_name_conv['muRCYR'] = "Muon Momentum Scale"
-    sys_name_conv['muIDsYR'] = "Muon Identification/Isolation"
-    sys_name_conv['muHLTsYR'] = "Muon Trigger"
-    sys_name_conv['muPrefYRC'] = "Muon Trigger Prefire Correction"
-    sys_name_conv['mufakesrwsYR'] = "Muon MisID Shape"
-    sys_name_conv['muPrefYRC'] = "Muon Trigger Prefire Correction"
-
-
-#sys_name_conv['METJECYR'] = "MET Uncertainties"
-#sys_name_conv['BTAGSYR'] = "b-tagging Uncertainty"
-
-
-
-
-
-def par_to_freezestr(par):
-    if("YRC" in par):
-        par16 = par.replace("YRC", "16")
-        par1718 = par.replace("YRC", "1718")
-        return par16 + "," + par1718
-    elif("YR" in par):
-        if("prefire" not in par):
-            par16 = par.replace("YR", "16")
-            par17 = par.replace("YR", "17")
-            par18 = par.replace("YR", "18")
-            return par16 + "," + par17 + "," + par18
-        else: #prefire only sys with no 18
-            return "prefire16,prefire17"
-    else:
-        return par
-
-        
-
-
-
-
-
-workspace = "workspaces/%s_%s_sys_uncs.root" % (chan, q)
-
-
-make_workspace(workspace, gen_level, chan, q, no_LQ , no_sys, fake_data, options.mLQ, year,False)
-print_and_do("combine -M MultiDimFit -d %s --saveFitResult --saveWorkspace -n _base --robustFit 1  %s" % (workspace, extra_params))
-
-
-if(options.expected):
-    print("Will inject AFB %.2f A0 %.2f for all toys " %(options.Afb, options.A0))
-
-
-    print_and_do(("combine -M GenerateOnly -d higgsCombine_base.MultiDimFit.mH120.root --snapshotName MultiDimFit --toysFrequentist"
-    " --bypassFrequentistFit --saveToys -t 1 -s %i  --setParameters Afb=%.2f,A0=%.2f")
-            % (s, options.Afb, options.A0))
-
-    print_and_do(("combine -M MultiDimFit -d %s -n _nom --saveWorkspace --saveFitResult --toysFile higgsCombineTest.GenerateOnly.mH120.%i.root " +
-    "--toysFrequentist  -t 1 --robustFit 1 --forceRecreateNLL %s") %(workspace, s, extra_params))
-
-    extra_params += " --toysFile higgsCombineTest.GenerateOnly.mH120.%i.root --toysFrequentist -t 1" % s
-
-else:
-    print_and_do("cp higgsCombine_base.MultiDimFit.mH120.root higgsCombine_nom.MultiDimFit.mH120.%i.root" % (s))
-
-print_and_do("""combine -M FitDiagnostics -d higgsCombine_nom.MultiDimFit.mH120.%i.root -w w --snapshotName MultiDimFit -n _nom1 %s""" % (s,extra_params))
-
-
-
-
-d = dict()
-n = 0
-for indi_par in individual_pars:
-    n+=1
-    freeze_str = par_to_freezestr(indi_par)
-    print_and_do("""combine -M FitDiagnostics --freezeParameters %s -d higgsCombine_nom.MultiDimFit.mH120.%i.root -w w --snapshotName MultiDimFit -n _%s %s """ 
-            % (freeze_str,s, indi_par, extra_params))
-    sys_unc = compute_sys("nom1", indi_par, -1)
-    d[indi_par] = sys_unc
-    #if(n>2): break
-
-for group_par in group_pars:
-    n+=1
-    freeze_str = par_to_freezestr(group_par)
-    print_and_do("""combine -M FitDiagnostics --freezeNuisanceGroups %s -d higgsCombine_nom.MultiDimFit.mH120.%i.root -w w --snapshotName MultiDimFit -n _%s %s """ % 
-            (freeze_str, s, group_par, extra_params))
-    sys_unc = compute_sys("nom1", group_par, -1)
-    d[group_par] = sys_unc
-    #if(n>4): break
-
-print(d)
-os.system("mkdir %s \n" % options.odir)
-with open("%s/%s_%s_sys_uncs.txt" % (options.odir, chan, q), 'w') as f_out:
-    sorted_d = sorted(d.items(), key=operator.itemgetter(1))
-    f_out.write("Systematic uncertainties (values x1000) for bin %i \n" % options.mbin)
-    for sys_name, val in sorted_d[::-1]:
-        if (sys_name in sys_name_conv.keys()):
-                out_name = sys_name_conv[sys_name]
+        if chan == "ee":
+            individual_pars = [ "dy_xsec", "db_xsec",  "top_xsec", "gam_xsec",  "elFakesYR",  "Pu", "prefireYR"]
+            group_pars =[  "RFscalesYRC", "emucostrwsYRC", "ptrwsYRC", "pdfs", "lumisYR","elScalesYR", "elHLTsYR", "elIDs", "elRECOs",  
+                            "elfakesrwsYR", "autoMCStats"] 
+            #"BTAGSYR","muPrefYRC","METJECYR",
         else:
-            out_name = sys_name
-        f_out.write("%s & %.2f \\\\ \n" % (out_name, val*1000.))
+            individual_pars = [ "dy_xsec", "db_xsec",  "top_xsec", "gam_xsec",  "muFakesYR", "Pu", "prefireYR",  "muRCYR", ]
+            group_pars =[  "RFscalesYRC", "emucostrwsYRC", "ptrwsYRC", "pdfs", "lumisYR","muIDsYR", "muHLTsYR", 
+                            "mufakesrwsYR",  "autoMCStats"] 
+
+        sys_name_conv = dict()
+
+        sys_name_conv['dy_xsec'] = "DY Cross Section"
+        sys_name_conv['db_xsec'] = "Diboson Cross Section"
+        sys_name_conv['top_xsec'] = "$\\ttbar$ Cross Section"
+        sys_name_conv['gam_xsec'] = "$\\PGg\\PGg$ Cross Section"
+        sys_name_conv['RFscalesYRC'] = "$\\alpha_s$ + Renormalization/Factorization Scales"
+        sys_name_conv['emucostrwsYRC'] = "$e\\mu$ Shape Corrections"
+        sys_name_conv['ptrwsYRC'] = "DY $p_{T}$ Correction"
+        sys_name_conv['pdfs'] = "PDFs"
+        sys_name_conv['lumisYR'] = "Luminosity"
+        sys_name_conv['autoMCStats'] = "MC and MisID Backgrounds Statistical Uncertainty"
+        sys_name_conv['Pu'] = "Pileup"
+
+        if chan == "ee": 
+            sys_name_conv['elFakesYR'] = "Electron MisID Normalization"
+            sys_name_conv['elScalesYR'] = "Electron Momentum Scale"
+            sys_name_conv['elHLTsYR'] = "Electron Trigger"
+            sys_name_conv['elIDs'] = "Electron Identification/Isolation"
+            sys_name_conv['elRECOs'] = "Electron Reconstruction"
+            sys_name_conv['elfakesrwsYR'] = "Electron MisID Shape"
+            sys_name_conv['prefireYR'] = "Electron Trigger Prefire Correction"
+
+        if chan== "mumu": 
+            sys_name_conv['muFakesYR'] = "Muon MisID Normalization"
+            sys_name_conv['muRCYR'] = "Muon Momentum Scale"
+            sys_name_conv['muIDsYR'] = "Muon Identification/Isolation"
+            sys_name_conv['muHLTsYR'] = "Muon Trigger"
+            sys_name_conv['muPrefYRC'] = "Muon Trigger Prefire Correction"
+            sys_name_conv['mufakesrwsYR'] = "Muon MisID Shape"
+            sys_name_conv['muPrefYRC'] = "Muon Trigger Prefire Correction"
 
 
-print_and_do("rm higgsCombine* fitDiagnostics* multidimfit*")
+        #sys_name_conv['METJECYR'] = "MET Uncertainties"
+        #sys_name_conv['BTAGSYR'] = "b-tagging Uncertainty"
+
+
+
+
+
+        def par_to_freezestr(par):
+            if("YRC" in par):
+                par16 = par.replace("YRC", "16")
+                par1718 = par.replace("YRC", "1718")
+                return par16 + "," + par1718
+            elif("YR" in par):
+                if("prefire" not in par):
+                    par16 = par.replace("YR", "16")
+                    par17 = par.replace("YR", "17")
+                    par18 = par.replace("YR", "18")
+                    return par16 + "," + par17 + "," + par18
+                else: #prefire only sys with no 18
+                    return "prefire16,prefire17"
+            else:
+                return par
+
+                
+
+
+
+
+
+        workspace = "workspaces/%s_%s_sys_uncs.root" % (chan, q)
+
+
+        make_workspace(workspace, gen_level, chan, q, no_LQ , no_sys, fake_data, options.mLQ, year,False)
+        print_and_do("combine -M MultiDimFit -d %s --saveFitResult --saveWorkspace -n _base --robustFit 1  %s" % (workspace, extra_params))
+
+
+        if(options.expected):
+            print("Will inject AFB %.2f A0 %.2f for all toys " %(options.Afb, options.A0))
+
+
+            print_and_do(("combine -M GenerateOnly -d higgsCombine_base.MultiDimFit.mH120.root --snapshotName MultiDimFit --toysFrequentist"
+            " --bypassFrequentistFit --saveToys -t 1 -s %i  --setParameters Afb=%.2f,A0=%.2f")
+                    % (s, options.Afb, options.A0))
+
+            print_and_do(("combine -M MultiDimFit -d %s -n _nom --saveWorkspace --saveFitResult --toysFile higgsCombineTest.GenerateOnly.mH120.%i.root " +
+            "--toysFrequentist  -t 1 --robustFit 1 --forceRecreateNLL %s") %(workspace, s, extra_params))
+
+            extra_params += " --toysFile higgsCombineTest.GenerateOnly.mH120.%i.root --toysFrequentist -t 1" % s
+
+        else:
+            print_and_do("cp higgsCombine_base.MultiDimFit.mH120.root higgsCombine_nom.MultiDimFit.mH120.%i.root" % (s))
+
+        print_and_do("""combine -M FitDiagnostics -d higgsCombine_nom.MultiDimFit.mH120.%i.root -w w --snapshotName MultiDimFit -n _nom1 %s""" % (s,extra_params))
+
+
+
+
+        d = dict()
+        n = 0
+        for indi_par in individual_pars:
+            n+=1
+            freeze_str = par_to_freezestr(indi_par)
+            print_and_do("""combine -M FitDiagnostics --freezeParameters %s -d higgsCombine_nom.MultiDimFit.mH120.%i.root -w w --snapshotName MultiDimFit -n _%s %s """ 
+                    % (freeze_str,s, indi_par, extra_params))
+            sys_unc = compute_sys("nom1", indi_par, -1)
+            d[indi_par] = sys_unc
+            #if(n>2): break
+
+        for group_par in group_pars:
+            n+=1
+            freeze_str = par_to_freezestr(group_par)
+            print_and_do("""combine -M FitDiagnostics --freezeNuisanceGroups %s -d higgsCombine_nom.MultiDimFit.mH120.%i.root -w w --snapshotName MultiDimFit -n _%s %s """ % 
+                    (freeze_str, s, group_par, extra_params))
+            sys_unc = compute_sys("nom1", group_par, -1)
+            d[group_par] = sys_unc
+            #if(n>4): break
+
+        print(d)
+        os.system("mkdir %s \n" % options.odir)
+        with open("%s/%s_%s_sys_uncs.txt" % (options.odir, chan, q), 'w') as f_out:
+            sorted_d = sorted(d.items(), key=operator.itemgetter(1))
+            f_out.write("Systematic uncertainties (values x1000) for bin %i \n" % options.mbin)
+            for sys_name, val in sorted_d[::-1]:
+                if (sys_name in sys_name_conv.keys()):
+                        out_name = sys_name_conv[sys_name]
+                else:
+                    out_name = sys_name
+                f_out.write("%s & %.2f \\\\ \n" % (out_name, val*1000.))
+
+
+        print_and_do("rm higgsCombine* fitDiagnostics* multidimfit*")
 
--- analyze/combine/AFB_fits/scripts/LQ_do_impacts.py
+++ analyze/combine/AFB_fits/scripts/LQ_do_impacts.py
@@ -17,158 +17,158 @@ parser.add_option("--diff", default = False, action="store_true",  help="Measure
 
 (options, args) = parser.parse_args()
 
-chan = "mumu"
-q = "d"
+for chan in ["mumu","ee"]:
+    for q in ["u","d"]:
 
-options.mLQ = 1000
-fake_data = True
-no_sys = False
-gen_level = False
-no_LQ = False
-year = -1
+        options.mLQ = 1000
+        fake_data = True
+        no_sys = False
+        gen_level = False
+        no_LQ = False
+        year = -1
 
-#extra_params = "--X-rtd MINIMIZER_no_analytic        
-extra_params = ""
+        #extra_params = "--X-rtd MINIMIZER_no_analytic        
+        extra_params = ""
 
-if chan=="ee":
-#all_sys =   ["METJEC", "BTAGCOR","BTAGUNCOR", "BTAGLIGHT" , 
-    all_sys =   ["elScaleSyst", "elScaleStat","elScaleGain", "elSmear", "Pu",
-                "elHLTBARPTHIGH", "elIDBARPTHIGH", "elRECOBARPTHIGH", "elHLTENDPTHIGH", "elIDENDPTHIGH", "elRECOENDPTHIGH",
-                "elHLTBARPTLOW", "elIDBARPTLOW", "elRECOBARPTLOW", "elHLTENDPTLOW", "elIDENDPTLOW", "elRECOENDPTLOW",
-                "ptrw1b", "ptrw2b", "ptrw3b", "ptrw4b", "ptrw5b", "ptrw6b", "ptrw7b",
-                "emucostrw1b", "emucostrw2b", "emucostrw3b", "emucostrw4b",
-                "elfakesrw1b", "elfakesrw2b", "elfakesrw3b", "elfakesrw4b",
-                "RENORM", "FAC", "REFAC", "alphaS", 
-                "dy_xsec","db_xsec","top_xsec","gam_xsec" ,"elFakes",
-                "lumiXY" ,"lumiLS" ,"lumiDB" ,"lumiBC", "lumiGS" ,"lumi", 
-                ]
-
-    correlate_all = ["elScaleSyst", "elSmear", "Pu", "elRECOBARPTHIGH", "elRECOENDPTHIGH", "elRECOBARPTLOW", "elRECOENDPTLOW",
-                     "elIDBARPTHIGH", "elIDENDPTHIGH", "elIDBARPTLOW", "elIDENDPTLOW", 
-                     "dy_xsec","db_xsec"  ,"top_xsec","gam_xsec",
-                     "lumiXY" ,"lumiLS" ,"lumiDB" ,"lumiBC" , "lumiGS",
-                     ] 
-
-    correlate_1718 = ["ptrw1b", "ptrw2b", "ptrw3b", "ptrw4b", "ptrw5b", "ptrw6b", "ptrw7b", 
+        if chan=="ee":
+        #all_sys =   ["METJEC", "BTAGCOR","BTAGUNCOR", "BTAGLIGHT" , 
+            all_sys =   ["elScaleSyst", "elScaleStat","elScaleGain", "elSmear", "Pu",
+                        "elHLTBARPTHIGH", "elIDBARPTHIGH", "elRECOBARPTHIGH", "elHLTENDPTHIGH", "elIDENDPTHIGH", "elRECOENDPTHIGH",
+                        "elHLTBARPTLOW", "elIDBARPTLOW", "elRECOBARPTLOW", "elHLTENDPTLOW", "elIDENDPTLOW", "elRECOENDPTLOW",
+                        "ptrw1b", "ptrw2b", "ptrw3b", "ptrw4b", "ptrw5b", "ptrw6b", "ptrw7b",
                         "emucostrw1b", "emucostrw2b", "emucostrw3b", "emucostrw4b",
-                        "RENORM", "FAC", "REFAC", "alphaS" ]
+                        "elfakesrw1b", "elfakesrw2b", "elfakesrw3b", "elfakesrw4b",
+                        "RENORM", "FAC", "REFAC", "alphaS", 
+                        "dy_xsec","db_xsec","top_xsec","gam_xsec" ,"elFakes",
+                        "lumiXY" ,"lumiLS" ,"lumiDB" ,"lumiBC", "lumiGS" ,"lumi", 
+                        ]
 
-else:
-    all_sys =   [ "muPref","muRC", "Pu",
-                "muHLTBAR", "muIDBAR", "muISOBAR",  "muHLTEND", "muIDEND", "muISOEND",  "muIDSYS", "muISOSYS",  
-                "ptrw1b", "ptrw2b", "ptrw3b", "ptrw4b", "ptrw5b", "ptrw6b", "ptrw7b",
-                "emucostrw1b", "emucostrw2b", "emucostrw3b", "emucostrw4b",
-                "mufakesrw1b", "mufakesrw2b", "mufakesrw3b", "mufakesrw4b",
-                "RENORM", "FAC", "REFAC", "alphaS", 
-                "dy_xsec","db_xsec","top_xsec","gam_xsec" ,"muFakes", 
-                "lumiXY" ,"lumiLS" ,"lumiDB" ,"lumiBC", "lumiGS" ,"lumi", 
-                ]
+            correlate_all = ["elScaleSyst", "elSmear", "Pu", "elRECOBARPTHIGH", "elRECOENDPTHIGH", "elRECOBARPTLOW", "elRECOENDPTLOW",
+                             "elIDBARPTHIGH", "elIDENDPTHIGH", "elIDBARPTLOW", "elIDENDPTLOW", 
+                             "dy_xsec","db_xsec"  ,"top_xsec","gam_xsec",
+                             "lumiXY" ,"lumiLS" ,"lumiDB" ,"lumiBC" , "lumiGS",
+                             ] 
 
-    correlate_all = [ "Pu", "muIDSYS", "muISOSYS",
-                     "dy_xsec","db_xsec"  ,"top_xsec","gam_xsec",
-                     "lumiXY" ,"lumiLS" ,"lumiDB" ,"lumiBC" , "lumiGS",
-                     ] 
+            correlate_1718 = ["ptrw1b", "ptrw2b", "ptrw3b", "ptrw4b", "ptrw5b", "ptrw6b", "ptrw7b", 
+                                "emucostrw1b", "emucostrw2b", "emucostrw3b", "emucostrw4b",
+                                "RENORM", "FAC", "REFAC", "alphaS" ]
 
-    correlate_1718 = ["ptrw1b", "ptrw2b", "ptrw3b", "ptrw4b", "ptrw5b", "ptrw6b", "ptrw7b", 
-                        "emucostrw1b", "emucostrw2b", "emucostrw3b", "emucostrw4b",
-                        "RENORM", "FAC", "REFAC", "alphaS" ]
-
-
-pars16 = []
-pars17 = []
-pars18 = []
-pars_comb = []
-
-for i in range(1,61):
-    pars_comb.append("pdf" + str(i))
-
-for par in all_sys:
-    if(par not in correlate_all): 
-        pars16.append(par + "16")
-        if(par not in correlate_1718):
-            pars17.append(par + "17")
-            pars18.append(par + "18")
         else:
-            pars_comb.append(par + "1718")
+            all_sys =   [ "muPref","muRC", "Pu",
+                        "muHLTBAR", "muIDBAR", "muISOBAR",  "muHLTEND", "muIDEND", "muISOEND",  "muIDSYS", "muISOSYS",  
+                        "ptrw1b", "ptrw2b", "ptrw3b", "ptrw4b", "ptrw5b", "ptrw6b", "ptrw7b",
+                        "emucostrw1b", "emucostrw2b", "emucostrw3b", "emucostrw4b",
+                        "mufakesrw1b", "mufakesrw2b", "mufakesrw3b", "mufakesrw4b",
+                        "RENORM", "FAC", "REFAC", "alphaS", 
+                        "dy_xsec","db_xsec","top_xsec","gam_xsec" ,"muFakes", 
+                        "lumiXY" ,"lumiLS" ,"lumiDB" ,"lumiBC", "lumiGS" ,"lumi", 
+                        ]
 
-    else:
-        pars_comb.append(par)
+            correlate_all = [ "Pu", "muIDSYS", "muISOSYS",
+                             "dy_xsec","db_xsec"  ,"top_xsec","gam_xsec",
+                             "lumiXY" ,"lumiLS" ,"lumiDB" ,"lumiBC" , "lumiGS",
+                             ] 
 
-
-
-pars16.append("prefire16")
-pars17.append("prefire17")
-#pars18.append("METHEM18")
-
-
-par_str = ""
-
-for par in (pars16 + pars17 + pars18 + pars_comb):
-    par_str += par +","
-
-#remove last comma
-par_str=par_str[:-1]
-
-ws_label = "%s_%s_impacts_mLQ%i" % (chan, q, options.mLQ)
-
-#if chan=="ee" and q=="u": yLQ_str = 'y_ue'
-#if chan=="ee" and q=="d": yLQ_str = 'y_ud'
-#if chan=="mumu" and q=="u": yLQ_str = 'y_um'
-#if chan=="mumu" and q=="d": yLQ_str = 'y_dm'
-
-yLQ2_str = 'yLQ2'
-A0_str = 'A0'
-Afb_str = 'Afb'
-if(options.diff):
-    A0_str = 'dA0'
-    Afb_str = 'dAfb'
-
-if(options.expected):
-    ws_label = "%s_%s_expected_impacts_mbin%i" % (chan, q, options.mbin)
+            correlate_1718 = ["ptrw1b", "ptrw2b", "ptrw3b", "ptrw4b", "ptrw5b", "ptrw6b", "ptrw7b", 
+                                "emucostrw1b", "emucostrw2b", "emucostrw3b", "emucostrw4b",
+                                "RENORM", "FAC", "REFAC", "alphaS" ]
 
 
+        pars16 = []
+        pars17 = []
+        pars18 = []
+        pars_comb = []
+
+        for i in range(1,61):
+            pars_comb.append("pdf" + str(i))
+
+        for par in all_sys:
+            if(par not in correlate_all): 
+                pars16.append(par + "16")
+                if(par not in correlate_1718):
+                    pars17.append(par + "17")
+                    pars18.append(par + "18")
+                else:
+                    pars_comb.append(par + "1718")
+
+            else:
+                pars_comb.append(par)
 
 
-workspace = "workspaces/%s.root" % (ws_label)
-#make_workspace(workspace, options.mbin, diff = options.diff)
-make_workspace(workspace, gen_level, chan, q, no_LQ , no_sys, fake_data, options.mLQ, year,False)
 
-print("Num pars = %i " % (len(pars16) + len(pars17) + len(pars18) + len(pars_comb)))
-print(par_str)
-
-s = 123456
-if(options.expected):
-    print("Will inject AFB %.2f A0 %.2f for all toys " %(options.Afb, options.A0))
-
-    #print_and_do("combine -M GenerateOnly -d initialFitWorkspace.root -n ToyGen -s %i --snapshotName initialFit --toysFrequentist --bypassFrequentistFit --saveToys -t 1  --setParameters Afb=%.2f,A0=%.2f" 
-    #    % (seed, options.Afb, options.A0))
-
-    if(not options.reuse_fit):
-        print_and_do("combine -M MultiDimFit -d %s --saveFitResult --saveWorkspace --robustFit 1 %s" % (workspace, extra_params))
-
-    print_and_do("combine -M GenerateOnly -d higgsCombineTest.MultiDimFit.mH120.root -s %i --snapshotName MultiDimFit --toysFrequentist --bypassFrequentistFit --saveToys -t 1  --setParameters Afb=%.2f,A0=%.2f" 
-            % (s, options.Afb, options.A0))
+        pars16.append("prefire16")
+        pars17.append("prefire17")
+        #pars18.append("METHEM18")
 
 
-    extra_params += " --toysFile higgsCombineTest.GenerateOnly.mH120.%i.root --toysFrequentist -t 1" % s
+        par_str = ""
+
+        for par in (pars16 + pars17 + pars18 + pars_comb):
+            par_str += par +","
+
+        #remove last comma
+        par_str=par_str[:-1]
+
+        ws_label = "%s_%s_impacts_mLQ%i" % (chan, q, options.mLQ)
+
+        #if chan=="ee" and q=="u": yLQ_str = 'y_ue'
+        #if chan=="ee" and q=="d": yLQ_str = 'y_ud'
+        #if chan=="mumu" and q=="u": yLQ_str = 'y_um'
+        #if chan=="mumu" and q=="d": yLQ_str = 'y_dm'
+
+        yLQ2_str = 'yLQ2'
+        A0_str = 'A0'
+        Afb_str = 'Afb'
+        if(options.diff):
+            A0_str = 'dA0'
+            Afb_str = 'dAfb'
+
+        if(options.expected):
+            ws_label = "%s_%s_expected_impacts_mbin%i" % (chan, q, options.mbin)
 
 
-print_and_do("combineTool.py -M Impacts -m 125 -d %s --doInitialFit --robustFit 1 %s " % (workspace, extra_params))
 
-if(options.expected):
-    print_and_do("cp higgsCombine_initialFit_Test.MultiDimFit.mH125.%i.root higgsCombine_initialFit_Test.MultiDimFit.mH125.root" % s)
 
-print_and_do("combineTool.py -M Impacts -m 125 -d %s --doFits --named %s --parallel %i %s" % (workspace, par_str, options.nThreads, extra_params))
+        workspace = "workspaces/%s.root" % (ws_label)
+        #make_workspace(workspace, options.mbin, diff = options.diff)
+        make_workspace(workspace, gen_level, chan, q, no_LQ , no_sys, fake_data, options.mLQ, year,False)
 
-if(options.expected):
-    print("Renaming sys fits")
-    for par in (pars16 + pars17 + pars18 + pars_comb):
-        os.system("cp higgsCombine_paramFit_Test_%s.MultiDimFit.mH125.%i.root higgsCombine_paramFit_Test_%s.MultiDimFit.mH125.root" % (par, s, par))
+        print("Num pars = %i " % (len(pars16) + len(pars17) + len(pars18) + len(pars_comb)))
+        print(par_str)
 
-print_and_do("combineTool.py -M Impacts -m 125 -d %s -o %s/%s.json --named %s" % (workspace, options.odir, ws_label, par_str))
-print_and_do("python scripts/my_plotImpacts.py -i %s/%s.json -o %s/%s_plot_yLQ2 --POI %s --blind" % (options.odir, ws_label, options.odir, ws_label, yLQ2_str))
-#print_and_do("python scripts/my_plotImpacts.py -i %s/%s.json -o %s/%s_plot_afb --POI %s --blind" % (options.odir, ws_label, options.odir, ws_label, Afb_str))
-#print_and_do("python scripts/my_plotImpacts.py -i %s/%s.json -o %s/%s_plot_a0 --POI %s --blind" % (options.odir, ws_label, options.odir, ws_label, A0_str))
-#print_and_do("rm higgsCombine_*")
+        s = 123456
+        if(options.expected):
+            print("Will inject AFB %.2f A0 %.2f for all toys " %(options.Afb, options.A0))
+
+            #print_and_do("combine -M GenerateOnly -d initialFitWorkspace.root -n ToyGen -s %i --snapshotName initialFit --toysFrequentist --bypassFrequentistFit --saveToys -t 1  --setParameters Afb=%.2f,A0=%.2f" 
+            #    % (seed, options.Afb, options.A0))
+
+            if(not options.reuse_fit):
+                print_and_do("combine -M MultiDimFit -d %s --saveFitResult --saveWorkspace --robustFit 1 %s" % (workspace, extra_params))
+
+            print_and_do("combine -M GenerateOnly -d higgsCombineTest.MultiDimFit.mH120.root -s %i --snapshotName MultiDimFit --toysFrequentist --bypassFrequentistFit --saveToys -t 1  --setParameters Afb=%.2f,A0=%.2f" 
+                    % (s, options.Afb, options.A0))
+
+
+            extra_params += " --toysFile higgsCombineTest.GenerateOnly.mH120.%i.root --toysFrequentist -t 1" % s
+
+
+        print_and_do("combineTool.py -M Impacts -m 125 -d %s --doInitialFit --robustFit 1 %s " % (workspace, extra_params))
+
+        if(options.expected):
+            print_and_do("cp higgsCombine_initialFit_Test.MultiDimFit.mH125.%i.root higgsCombine_initialFit_Test.MultiDimFit.mH125.root" % s)
+
+        print_and_do("combineTool.py -M Impacts -m 125 -d %s --doFits --named %s --parallel %i %s" % (workspace, par_str, options.nThreads, extra_params))
+
+        if(options.expected):
+            print("Renaming sys fits")
+            for par in (pars16 + pars17 + pars18 + pars_comb):
+                os.system("cp higgsCombine_paramFit_Test_%s.MultiDimFit.mH125.%i.root higgsCombine_paramFit_Test_%s.MultiDimFit.mH125.root" % (par, s, par))
+
+        print_and_do("combineTool.py -M Impacts -m 125 -d %s -o %s/%s.json --named %s" % (workspace, options.odir, ws_label, par_str))
+        print_and_do("python scripts/my_plotImpacts.py -i %s/%s.json -o %s/%s_plot_yLQ2 --POI %s --blind" % (options.odir, ws_label, options.odir, ws_label, yLQ2_str))
+        #print_and_do("python scripts/my_plotImpacts.py -i %s/%s.json -o %s/%s_plot_afb --POI %s --blind" % (options.odir, ws_label, options.odir, ws_label, Afb_str))
+        #print_and_do("python scripts/my_plotImpacts.py -i %s/%s.json -o %s/%s_plot_a0 --POI %s --blind" % (options.odir, ws_label, options.odir, ws_label, A0_str))
+        #print_and_do("rm higgsCombine_*")
 
 
