e9e75c871eff4a01c5ea3964e5de9fea210e2508
Sanjana <sanjanasekhar39@gmail.com>
2022-06-15 16:21:35 -0500

    redoing SM gen fit

--- analyze/combine/AFB_fits/scripts/LQ_do_fits.py
+++ analyze/combine/AFB_fits/scripts/LQ_do_fits.py
@@ -75,7 +75,7 @@ for y in [2017]:
 
 
             #for mLQ in [1500]:
-            for mLQ in [2500]:
+            for mLQ in [1000]:
             #,1500,2000,2500,3000,3500,4000,4500,5000,5500,6000]:
             #mLQ = 1000.
             #for mbin in range(bin_start, bin_stop):
