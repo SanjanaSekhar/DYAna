b2868120581b2871e999e118f632f56ba6093d43
Sanjana <sanjanasekhar39@gmail.com>
2023-03-24 15:57:32 -0500

    MCStatBin plots

--- plots/Fitting/LQ_draw_mcstats_templates.py
+++ plots/Fitting/LQ_draw_mcstats_templates.py
@@ -3,8 +3,11 @@ from ROOT import *
 from optparse import OptionParser
 import sys
 
+gStyle.SetOptStat(0)
+gROOT.SetBatch(1)
+
 ext = "020923"
-mLQ = 2500
+mLQ = 2000
 odir = "AN_plots/Systematics/UpDown/"
 x_start = 0.7;
 x_end = 0.9;
@@ -12,16 +15,22 @@ y_start = 0.75;
 y_end = 0.9;
 
 for year in [2016,2017,2018]:
-
+    print("year is %i",year)
     fin = "../analyze/combine/templates/LQm%i_merge_templates%i_%s.root"%(mLQ,year-2000,ext)
 
     f = ROOT.TFile.Open(fin, "READ")
     f.cd("LQ")
     keys = ROOT.gDirectory.GetListOfKeys().Clone()
-    ee_sys_list = [s for s in keys if 'ee%i_fpl_MCStatBin'%(year-2000) in s]
-    mumu_sys_list = [s for s in keys if 'mumu%i_fpl_MCStatBin'%(year-2000) in s]
+    ee_sys_list, mumu_sys_list = [],[]
+    for k in keys:
+        if 'ee%i_fpl_MCStatBin'%(year-2000) in k.GetName(): ee_sys_list.append(k.GetName())
+        if 'mumu%i_fpl_MCStatBin'%(year-2000) in k.GetName(): mumu_sys_list.append(k.GetName())
+    #ee_sys_list = [s.GetName() for s.GetName() in keys if 'ee%i_fpl_MCStatBin'%(year-2000) in s.GetName()]
+    #mumu_sys_list = [s.GetName() for s.GetName() in keys if 'mumu%i_fpl_MCStatBin'%(year-2000) in s.GetName()]
+    print(ee_sys_list)
     for sys in ee_sys_list:
         if 'Up' in sys:
+	    print("Drawing %s"%sys)
             h_up = ROOT.gDirectory.Get(sys)
             h_down = ROOT.gDirectory.Get(sys[:-2]+"Down")
             h = ROOT.gDirectory.Get("ee%i_fpl"%(year-2000))
@@ -41,10 +50,11 @@ for year in [2016,2017,2018]:
             leg1.AddEntry(h, "Nominal Template", "l");
             leg1.AddEntry(h_up, "Sys Up Template", "l");
             leg1.AddEntry(h_down, "Sys Down Template", "l");
-            c1.Print("%s/ee_fpl_MCStatBin.pdf"%odir)
-
+            c1.Print("%s/ee%i_fpl_MCStatBin.pdf"%(odir,year-2000))
+    print(mumu_sys_list)
     for sys in mumu_sys_list:
         if 'Up' in sys:
+            print("Drawing %s"%sys)
             h_up = ROOT.gDirectory.Get(sys)
             h_down = ROOT.gDirectory.Get(sys[:-2]+"Down")
             h = ROOT.gDirectory.Get("mumu%i_fpl"%(year-2000))
@@ -64,6 +74,6 @@ for year in [2016,2017,2018]:
             leg1.AddEntry(h, "Nominal Template", "l");
             leg1.AddEntry(h_up, "Sys Up Template", "l");
             leg1.AddEntry(h_down, "Sys Down Template", "l");
-            c1.Print("%s/mumu_fpl_MCStatBin.pdf"%odir)
+            c1.Print("%s/mumu%i_fpl_MCStatBin.pdf"%(odir,year-2000))
 
-
+    f.Close()
