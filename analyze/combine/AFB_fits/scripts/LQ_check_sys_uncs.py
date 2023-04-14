942cd700809ce26fa8747178154cdcc180eef5ac
Sanjana Sekhar <ssekhar1@jhu.edu>
2023-04-14 14:39:25 -0400

    updating sys fracs scripts for mc stats

--- analyze/combine/AFB_fits/scripts/LQ_check_sys_uncs.py
+++ analyze/combine/AFB_fits/scripts/LQ_check_sys_uncs.py
@@ -14,177 +14,187 @@ parser.add_option("--A0",  default=0.05, type='float', help="A0 value to inject 
 parser.add_option("-o", "--odir", default="sys_uncs/", help = "output directory")
 parser.add_option("--reuse_fit", default=False, action="store_true", help="Reuse initial fit from previous run to save time")
 parser.add_option("--diff", default=False, action="store_true", help="Diff")
-
+parser.add_option("--mLQ",  default=2000, type='int', help="mLQ")
+parser.add_option("--vec",  default=False, help="is vec?")
+parser.add_option("--chan",  default="ee", help="channel ee or mumu ")
+parser.add_option("--q",  default="u", help=" channel u,d")
+parser.add_option("--hadd",  default=False, help="hadd")
+parser.add_option("--ending", default="041123", help="date")
 (options, args) = parser.parse_args()
 
-for chan in ["ee","mumu"]:
-    for q in ["u","d"]:
-        options.mLQ = 2000
-        fake_data = True
-        no_sys = False
-        gen_level = False
-        no_LQ = False
-        year = -1
-	is_vec = True
-        extra_params = "--X-rtd MINIMIZER_no_analytic"
-	ending = "021423"
-        s = 3456
-	if is_vec: ending+="_vec"
-        extra_params += " -s %i" % s
+chan = options.chan
+q = options.q
+fake_data = True
+no_sys = False
+gen_level = False
+no_LQ = False
+year = -1
+is_vec = options.vec
+extra_params = "--X-rtd MINIMIZER_no_analytic"
+ending = options.ending
+s = 3456
+if is_vec: ending+="_vec"
+extra_params += " -s %i" % s
 
-        if chan == "ee":
-	    
-            individual_pars = ["nlo_sys", "dy_xsec", "db_xsec",  "top_xsec", "gam_xsec",  "elFakesYR",  "Pu", "prefireYR"]
-            
-	    group_pars =[  "RFscalesYRC", "emucostrwsYRC",  "pdfs", "lumisYR","elScalesYR", "elHLTsYR", "elIDs", "elRECOs",  
-                            "elfakesrwsYR", "autoMCStats"] 
-            #"BTAGSYR","muPrefYRC","METJECYR",
-            '''
-	    #individual_pars = ["nlo_sys","dy_xsec","gam_xsec","top_xsec","db_xsec"]
-	    group_pars = []
-	    '''
+if options.hadd:
+	print_and_do("xrdcp -f root://cmseos.fnal.gov//store/user/ssekhar/Condor_outputs/sys_%s_%s%s/%s_%s_m%s_sys_uncs_%s.txt sys_uncs/"%(chan, q, ("_vec" if is_vec else ""), chan, q, options.mLQ, ending))
+
+else:
+	if chan == "ee":
+
+		individual_pars = ["nlo_sys", "dy_xsec", "db_xsec",  "top_xsec", "gam_xsec",  "elFakesYR",  "Pu", "prefireYR"]
+		
+		group_pars =[  "RFscalesYRC", "emucostrwsYRC",  "pdfs", "lumisYR","elScalesYR", "elHLTsYR", "elIDs", "elRECOs",  
+						"elfakesrwsYR", "autoMCStats,MCStatBin"] 
+		#"BTAGSYR","muPrefYRC","METJECYR",
+		'''
+	#individual_pars = ["nlo_sys","dy_xsec","gam_xsec","top_xsec","db_xsec"]
+	group_pars = []
+	'''
 	else:
-            individual_pars = ["nlo_sys", "dy_xsec", "db_xsec",  "top_xsec", "gam_xsec",  "muFakesYR", "Pu", "muPrefYRC",  "muRCYR", ]
-            group_pars =[  "RFscalesYRC", "emucostrwsYRC", "pdfs", "lumisYR","muIDsYR", "muHLTsYR", 
-                            "mufakesrwsYR",  "autoMCStats"] 
+		individual_pars = ["nlo_sys", "dy_xsec", "db_xsec",  "top_xsec", "gam_xsec",  "muFakesYR", "Pu", "muPrefYRC",  "muRCYR", ]
+		group_pars =[  "RFscalesYRC", "emucostrwsYRC", "pdfs", "lumisYR","muIDsYR", "muHLTsYR", 
+						"mufakesrwsYR",  "autoMCStats"] 
 
-        sys_name_conv = dict()
+	sys_name_conv = dict()
 	sys_name_conv['nlo_sys'] = "LQ LO reweighting"
-        sys_name_conv['dy_xsec'] = "DY Cross Section"
-        sys_name_conv['db_xsec'] = "Diboson Cross Section"
-        sys_name_conv['top_xsec'] = "$\\ttbar$ Cross Section"
-        sys_name_conv['gam_xsec'] = "$\\PGg\\PGg$ Cross Section"
-        sys_name_conv['RFscalesYRC'] = "$\\alpha_s$ + Renormalization/Factorization Scales"
-        sys_name_conv['emucostrwsYRC'] = "$e\\mu$ Shape Corrections"
-        #sys_name_conv['ptrwsYRC'] = "DY $p_{T}$ Correction"
-        sys_name_conv['pdfs'] = "PDFs"
-        sys_name_conv['lumisYR'] = "Luminosity"
-        sys_name_conv['autoMCStats'] = "MC and MisID Backgrounds Statistical Uncertainty"
-        sys_name_conv['Pu'] = "Pileup"
+	sys_name_conv['dy_xsec'] = "DY Cross Section"
+	sys_name_conv['db_xsec'] = "Diboson Cross Section"
+	sys_name_conv['top_xsec'] = "$\\ttbar$ Cross Section"
+	sys_name_conv['gam_xsec'] = "$\\PGg\\PGg$ Cross Section"
+	sys_name_conv['RFscalesYRC'] = "$\\alpha_s$ + Renormalization/Factorization Scales"
+	sys_name_conv['emucostrwsYRC'] = "$e\\mu$ Shape Corrections"
+	#sys_name_conv['ptrwsYRC'] = "DY $p_{T}$ Correction"
+	sys_name_conv['pdfs'] = "PDFs"
+	sys_name_conv['lumisYR'] = "Luminosity"
+	sys_name_conv['autoMCStats,MCStatBin'] = "MC and MisID Backgrounds Statistical Uncertainty"
+	sys_name_conv['Pu'] = "Pileup"
 
-        if chan == "ee": 
-            sys_name_conv['elFakesYR'] = "Electron MisID Normalization"
-            sys_name_conv['elScalesYR'] = "Electron Momentum Scale"
-            sys_name_conv['elHLTsYR'] = "Electron Trigger"
-            sys_name_conv['elIDs'] = "Electron Identification/Isolation"
-            sys_name_conv['elRECOs'] = "Electron Reconstruction"
-            sys_name_conv['elfakesrwsYR'] = "Electron MisID Shape"
-            sys_name_conv['prefireYR'] = "Electron Trigger Prefire Correction"
+	if chan == "ee": 
+		sys_name_conv['elFakesYR'] = "Electron MisID Normalization"
+		sys_name_conv['elScalesYR'] = "Electron Momentum Scale"
+		sys_name_conv['elHLTsYR'] = "Electron Trigger"
+		sys_name_conv['elIDs'] = "Electron Identification/Isolation"
+		sys_name_conv['elRECOs'] = "Electron Reconstruction"
+		sys_name_conv['elfakesrwsYR'] = "Electron MisID Shape"
+		sys_name_conv['prefireYR'] = "Electron Trigger Prefire Correction"
 
-        if chan== "mumu": 
-            sys_name_conv['muFakesYR'] = "Muon MisID Normalization"
-            sys_name_conv['muRCYR'] = "Muon Momentum Scale"
-            sys_name_conv['muIDsYR'] = "Muon Identification/Isolation"
-            sys_name_conv['muHLTsYR'] = "Muon Trigger"
-            sys_name_conv['muPrefYRC'] = "Muon Trigger Prefire Correction"
-            sys_name_conv['mufakesrwsYR'] = "Muon MisID Shape"
-            sys_name_conv['muPrefYRC'] = "Muon Trigger Prefire Correction"
-
-
-        #sys_name_conv['METJECYR'] = "MET Uncertainties"
-        #sys_name_conv['BTAGSYR'] = "b-tagging Uncertainty"
+	if chan== "mumu": 
+		sys_name_conv['muFakesYR'] = "Muon MisID Normalization"
+		sys_name_conv['muRCYR'] = "Muon Momentum Scale"
+		sys_name_conv['muIDsYR'] = "Muon Identification/Isolation"
+		sys_name_conv['muHLTsYR'] = "Muon Trigger"
+		sys_name_conv['muPrefYRC'] = "Muon Trigger Prefire Correction"
+		sys_name_conv['mufakesrwsYR'] = "Muon MisID Shape"
+		sys_name_conv['muPrefYRC'] = "Muon Trigger Prefire Correction"
 
 
+	#sys_name_conv['METJECYR'] = "MET Uncertainties"
+	#sys_name_conv['BTAGSYR'] = "b-tagging Uncertainty"
 
 
 
-        def par_to_freezestr(par):
-            if("YRC" in par):
-                par16 = par.replace("YRC", "16")
-                par1718 = par.replace("YRC", "1718")
-                return par16 + "," + par1718
-            elif("YR" in par):
-    
+
+
+	def par_to_freezestr(par):
+		if("YRC" in par):
+			par16 = par.replace("YRC", "16")
+			par1718 = par.replace("YRC", "1718")
+			return par16 + "," + par1718
+		elif("YR" in par):
+
 		if  "prefire" not in par:
-                    par16 = par.replace("YR", "16")
-                    par17 = par.replace("YR", "17")
-                    par18 = par.replace("YR", "18")
-                    return par16 + "," + par17 + "," + par18
-                else: #prefire only sys with no 18
-                    if chan=="ee":   return "prefire16,prefire17"
-            else:
-                return par
+				par16 = par.replace("YR", "16")
+				par17 = par.replace("YR", "17")
+				par18 = par.replace("YR", "18")
+				return par16 + "," + par17 + "," + par18
+			else: #prefire only sys with no 18
+				if chan=="ee":   return "prefire16,prefire17"
+		else:
+			return par
 
-                
-
-
+			
 
 
 
-        workspace = "workspaces/%s_%s_sys_uncs_m%i.root" % (chan, q, options.mLQ)
 
 
-        make_workspace(workspace, gen_level, chan, q, is_vec, no_LQ , no_sys, fake_data, options.mLQ, year,False, False)
-        print_and_do("combine -M MultiDimFit -d %s --saveFitResult --saveWorkspace -n _base --robustFit 1  %s" % (workspace, extra_params))
+	workspace = "workspaces/%s_%s_sys_uncs_m%i.root" % (chan, q, options.mLQ)
+
+
+	make_workspace(workspace, gen_level, chan, q, is_vec, no_LQ , no_sys, fake_data, options.mLQ, year,True, False)
+	print_and_do("combine -M MultiDimFit -d %s --saveFitResult --saveWorkspace -n _base --robustFit 1  %s" % (workspace, extra_params))
 	#print_and_do("combine -M FitDiagnostics -d %s  --saveWorkspace -n _base --robustFit 1  %s"     % (workspace, extra_params))
 
-        if(options.expected):
-            print("Will inject AFB %.2f A0 %.2f for all toys " %(options.Afb, options.A0))
+	if(options.expected):
+		print("Will inject AFB %.2f A0 %.2f for all toys " %(options.Afb, options.A0))
 
 
-            print_and_do(("combine -M GenerateOnly -d higgsCombine_base.MultiDimFit.mH120.root --snapshotName MultiDimFit --toysFrequentist"
-            " --bypassFrequentistFit --saveToys -t 1 -s %i  --setParameters Afb=%.2f,A0=%.2f")
-                    % (s, options.Afb, options.A0))
+		print_and_do(("combine -M GenerateOnly -d higgsCombine_base.MultiDimFit.mH120.root --snapshotName MultiDimFit --toysFrequentist"
+		" --bypassFrequentistFit --saveToys -t 1 -s %i  --setParameters Afb=%.2f,A0=%.2f")
+				% (s, options.Afb, options.A0))
 
-            print_and_do(("combine -M MultiDimFit -d %s -n _nom --saveWorkspace --saveFitResult --toysFile higgsCombineTest.GenerateOnly.mH120.%i.root " +
-            "--toysFrequentist  -t 1 --robustFit 1 --forceRecreateNLL %s") %(workspace, s, extra_params))
+		print_and_do(("combine -M MultiDimFit -d %s -n _nom --saveWorkspace --saveFitResult --toysFile higgsCombineTest.GenerateOnly.mH120.%i.root " +
+		"--toysFrequentist  -t 1 --robustFit 1 --forceRecreateNLL %s") %(workspace, s, extra_params))
 
-            extra_params += " --toysFile higgsCombineTest.GenerateOnly.mH120.%i.root --toysFrequentist -t 1" % s
+		extra_params += " --toysFile higgsCombineTest.GenerateOnly.mH120.%i.root --toysFrequentist -t 1" % s
 
-        else:
-            print_and_do("cp higgsCombine_base.MultiDimFit.mH120.%i.root higgsCombine_nom.MultiDimFit.mH120.%i.root" % (s,s))
-	    #print_and_do("cp higgsCombine_base.FitDiagnostics.mH120.%i.root higgsCombine_nom.FitDiagnostics.mH120.%i.root" % (s,s))
-	    print_and_do("cp multidimfit_base.root multidimfit_nom.root")
-        #print_and_do("""combine -M FitDiagnostics -d higgsCombine_nom.MultiDimFit.mH120.%i.root -w w --snapshotName MultiDimFit -n _nom1 %s """ % (s,extra_params))
+	else:
+		print_and_do("cp higgsCombine_base.MultiDimFit.mH120.%i.root higgsCombine_nom.MultiDimFit.mH120.%i.root" % (s,s))
+	#print_and_do("cp higgsCombine_base.FitDiagnostics.mH120.%i.root higgsCombine_nom.FitDiagnostics.mH120.%i.root" % (s,s))
+	print_and_do("cp multidimfit_base.root multidimfit_nom.root")
+	#print_and_do("""combine -M FitDiagnostics -d higgsCombine_nom.MultiDimFit.mH120.%i.root -w w --snapshotName MultiDimFit -n _nom1 %s """ % (s,extra_params))
 	#print_and_do("""combine -M FitDiagnostics -d higgsCombine_nom.FitDiagnostics.mH120.%i.root -w w --snapshotName FitDiagnostics -n _nom1 %s """ % (s,extra_params))
 
 
 
-        d = dict()
-        n = 0
-        for indi_par in individual_pars:
-            n+=1
-            freeze_str = par_to_freezestr(indi_par)
-            #print_and_do("""combine -M FitDiagnostics --freezeParameters %s -d higgsCombine_nom.MultiDimFit.mH120.%i.root -w w --snapshotName MultiDimFit --robustFit 1 -n _%s %s""" % (freeze_str,s, indi_par, extra_params))
-                  #  % (freeze_str,s, indi_par, extra_params))
-            #print_and_do("""combine -M FitDiagnostics --freezeParameters %s -d higgsCombine_nom.FitDiagnostics.mH120.%i.root -w w  --robustFit 1 -n _%s %s""" % (freeze_str,s, indi_par, extra_params))
-	    print_and_do("""combine -M MultiDimFit --freezeParameters %s -d higgsCombine_nom.MultiDimFit.mH120.%i.root  --saveFitResult --robustFit 1 -n _%s %s""" % (freeze_str,s, indi_par, extra_params))
-	    sys_unc = compute_sys("nom", indi_par, s)
-            #sys_unc = compute_sys("nom", indi_par, s)
-            d[indi_par] = sys_unc
-            #if(n>2): break
+	d = dict()
+	n = 0
+	for indi_par in individual_pars:
+		n+=1
+		freeze_str = par_to_freezestr(indi_par)
+		#print_and_do("""combine -M FitDiagnostics --freezeParameters %s -d higgsCombine_nom.MultiDimFit.mH120.%i.root -w w --snapshotName MultiDimFit --robustFit 1 -n _%s %s""" % (freeze_str,s, indi_par, extra_params))
+			  #  % (freeze_str,s, indi_par, extra_params))
+		#print_and_do("""combine -M FitDiagnostics --freezeParameters %s -d higgsCombine_nom.FitDiagnostics.mH120.%i.root -w w  --robustFit 1 -n _%s %s""" % (freeze_str,s, indi_par, extra_params))
+		print_and_do("""combine -M MultiDimFit --freezeParameters %s -d higgsCombine_nom.MultiDimFit.mH120.%i.root  --saveFitResult --robustFit 1 -n _%s %s""" % (freeze_str,s, indi_par, extra_params))
+		sys_unc = compute_sys("nom", indi_par, s)
+		#sys_unc = compute_sys("nom", indi_par, s)
+		d[indi_par] = sys_unc
+		#if(n>2): break
 
-        for group_par in group_pars:
-            n+=1
-            freeze_str = par_to_freezestr(group_par)
-            #print_and_do("""combine -M MultiDimFit --freezeNuisanceGroups %s -d higgsCombine_nom.MultiDimFit.mH120.%i.root -w w --snapshotName MultiDimFit --robustFit 1 -n _%s %s""" % 
-            #       (freeze_str, s, group_par, extra_params))
-            #print_and_do("""combine -M FitDiagnostics --freezeNuisanceGroups %s -d higgsCombine_nom.FitDiagnostics.mH120.%i.root -w w  --robustFit 1 -n _%s %s""" %  (freeze_str, s, group_par, extra_params))
-	    print_and_do("""combine -M MultiDimFit --freezeNuisanceGroups %s -d higgsCombine_nom.MultiDimFit.mH120.%i.root  --saveFitResult --robustFit 1 -n _%s %s""" %(freeze_str, s, group_par, extra_params))
-	    sys_unc = compute_sys("nom", group_par, s)
-            #sys_unc = compute_sys("nom", indi_par, s)
-            d[group_par] =sys_unc
-            #if(n>4): break
+	for group_par in group_pars:
+		n+=1
+		freeze_str = par_to_freezestr(group_par)
+		#print_and_do("""combine -M MultiDimFit --freezeNuisanceGroups %s -d higgsCombine_nom.MultiDimFit.mH120.%i.root -w w --snapshotName MultiDimFit --robustFit 1 -n _%s %s""" % 
+		#       (freeze_str, s, group_par, extra_params))
+		#print_and_do("""combine -M FitDiagnostics --freezeNuisanceGroups %s -d higgsCombine_nom.FitDiagnostics.mH120.%i.root -w w  --robustFit 1 -n _%s %s""" %  (freeze_str, s, group_par, extra_params))
+		print_and_do("""combine -M MultiDimFit --freezeNuisanceGroups %s -d higgsCombine_nom.MultiDimFit.mH120.%i.root  --saveFitResult --robustFit 1 -n _%s %s""" %(freeze_str, s, group_par, extra_params))
+		sys_unc = compute_sys("nom", group_par, s)
+		#sys_unc = compute_sys("nom", indi_par, s)
+		d[group_par] =sys_unc
+		#if(n>4): break
 
-        print(d)
+	print(d)
 	sum_uncs2 = 0
-        os.system("mkdir %s \n" % options.odir)
-        with open("%s/%s_%s_m%s_sys_uncs_%s.txt" % (options.odir, chan, q, options.mLQ,ending), 'w') as f_out:
-            sorted_d = sorted(d.items(), key=operator.itemgetter(1))
-            f_out.write("Systematic uncertainties for bin %i\n" % options.mbin)
-            for sys_name, val in sorted_d[::-1]:
-                if (sys_name in sys_name_conv.keys()):
-                        out_name = sys_name_conv[sys_name]
-                else:
-                    out_name = sys_name
-		sum_uncs2 += (val*val)
+	os.system("mkdir %s \n" % options.odir)
+	with open("%s/%s_%s_m%s_sys_uncs_%s.txt" % (options.odir, chan, q, options.mLQ,ending), 'w') as f_out:
+		sorted_d = sorted(d.items(), key=operator.itemgetter(1))
+		f_out.write("Systematic uncertainties for bin %i\n" % options.mbin)
+		
+		for sys_name, val in sorted_d[::-1]:
+			# if (sys_name in sys_name_conv.keys()):
+			# 	out_name = sys_name_conv[sys_name]
+			# else:
+			# 	out_name = sys_name
+			sum_uncs2 += (val*val)
 
-	    for sys_name, val in sorted_d[::-1]:
-                if (sys_name in sys_name_conv.keys()):
-                        out_name = sys_name_conv[sys_name]
-                else:
-                    out_name = sys_name
-                f_out.write("%s & %.5f & %.2f  \\\\ \n" % (out_name, val, ((val*val)*100)/sum_uncs2))
-	    f_out.write("Total Uncertainty & %.2f  \\\\ \n" % (np.sqrt(sum_uncs2)))
+		for sys_name, val in sorted_d[::-1]:
+			if (sys_name in sys_name_conv.keys()):
+				out_name = sys_name_conv[sys_name]
+			else:
+				out_name = sys_name
+			f_out.write("%s & %.5f & %.2f  \\\\ \n" % (out_name, val, ((val*val)*100)/sum_uncs2))
+		
+		f_out.write("Total Uncertainty & %.2f  \\\\ \n" % (np.sqrt(sum_uncs2)))
 
-        #print_and_do("rm higgsCombine* fitDiagnostics* multidimfit*")
+	#print_and_do("rm higgsCombine* fitDiagnostics* multidimfit*")
--- analyze/combine/AFB_fits/scripts/add_group_impact.py
+++ analyze/combine/AFB_fits/scripts/add_group_impact.py
@@ -10,78 +10,78 @@ from ROOT import TFile
 from pprint import pprint
 
 def getTripletFromFile(fName):
-    f = TFile.Open(fName)
-    # f.Print()
-    t = f.Get('limit')
-    # t.Scan('*')
-    vals = []
-    for e in t:
-        if e.quantileExpected == -1:
-            continue
-        vals.append( (e.quantileExpected, e.limit) )
+	f = TFile.Open(fName)
+	# f.Print()
+	t = f.Get('limit')
+	# t.Scan('*')
+	vals = []
+	for e in t:
+		if e.quantileExpected == -1:
+			continue
+		vals.append( (e.quantileExpected, e.limit) )
 
-    # print vals
-    return UncertaintiesFromTriplet(vals)
+	# print vals
+	return UncertaintiesFromTriplet(vals)
 
 def UncertaintiesFromTriplet(triplet):
-    #This assumes that the values are ordered as central(0),lower(1),upper(2)
-    #This could be found from the quantileExpected value, but I was lazy
-    #up = triplet[2][1] - triplet[0][1]
-    #dn = triplet[1][1] - triplet[0][1]
-    # print dn, up
-    up = triplet[2] - triplet[0]
-    dn = triplet[1] - triplet[0]
-    return (dn, up)
+	#This assumes that the values are ordered as central(0),lower(1),upper(2)
+	#This could be found from the quantileExpected value, but I was lazy
+	#up = triplet[2][1] - triplet[0][1]
+	#dn = triplet[1][1] - triplet[0][1]
+	# print dn, up
+	up = triplet[2] - triplet[0]
+	dn = triplet[1] - triplet[0]
+	return (dn, up)
 
 from math import sqrt
 def quadDiffPair(unc1, unc2):
-    dn = sqrt( abs(unc1[0]**2 - unc2[0]**2 ))
-    up = sqrt( abs(unc1[1]**2 - unc2[1]**2) )
-    return (-dn, up)
+	dn = sqrt( abs(unc1[0]**2 - unc2[0]**2 ))
+	up = sqrt( abs(unc1[1]**2 - unc2[1]**2) )
+	return (-dn, up)
 
 def quadDiffAvg(unc1, unc2):
-    dn = sqrt( abs(unc1[0]**2 - unc2[0]**2 ))
-    up = sqrt( abs(unc1[1]**2 - unc2[1]**2) )
-    return (dn + up)/2.0
+	dn = sqrt( abs(unc1[0]**2 - unc2[0]**2 ))
+	up = sqrt( abs(unc1[1]**2 - unc2[1]**2) )
+	return (dn + up)/2.0
 
 def compute_sys(nom_l, sys_l, seed = -1):
-    if(seed <0):
-        sys_f = "higgsCombine_%s.FitDiagnostics.mH120.root" % sys_l
-        nom_f = "higgsCombine_%s.FitDiagnostics.mH120.root" % nom_l
-    else:
-        #sys_f = "higgsCombine_%s.FitDiagnostics.mH120.%i.root" % (sys_l,seed)
-        #nom_f = "higgsCombine_%s.FitDiagnostics.mH120.%i.root" % (nom_l,seed)
+	if(seed <0):
+		sys_f = "higgsCombine_%s.FitDiagnostics.mH120.root" % sys_l
+		nom_f = "higgsCombine_%s.FitDiagnostics.mH120.root" % nom_l
+	else:
+		#sys_f = "higgsCombine_%s.FitDiagnostics.mH120.%i.root" % (sys_l,seed)
+		#nom_f = "higgsCombine_%s.FitDiagnostics.mH120.%i.root" % (nom_l,seed)
 	#sys_f = "higgsCombine_%s.MultiDimFit.mH120.%i.root" % (sys_l,seed)
-        #nom_f = "higgsCombine_%s.MultiDimFit.mH120.%i.root" % (nom_l,seed)
-	sys_f = "multidimfit_%s.root" % (sys_l)
-        nom_f = "multidimfit_%s.root" % (nom_l)
-    
-    _file0 = TFile.Open(nom_f)
-    fit_mdf = _file0.Get("fit_mdf")
-    nom = fit_mdf.floatParsFinal()
-    nom_index_yLQ2 = nom.index("yLQ2")
-    nom_vals = nom.at(nom_index_yLQ2)
-    nom_unc = []
-    nom_unc.append(nom_vals.getValV())
-    nom_unc.append(nom_vals.getErrorLo())
-    nom_unc.append(nom_vals.getErrorHi())
-    nom_unc = UncertaintiesFromTriplet(nom_unc)
-    
+		#nom_f = "higgsCombine_%s.MultiDimFit.mH120.%i.root" % (nom_l,seed)
+		sys_f = "multidimfit_%s.root" % (sys_l)
+		nom_f = "multidimfit_%s.root" % (nom_l)
+	
+	_file0 = TFile.Open(nom_f)
+	fit_mdf = _file0.Get("fit_mdf")
+	nom = fit_mdf.floatParsFinal()
+	nom_index_yLQ2 = nom.index("yLQ2")
+	nom_vals = nom.at(nom_index_yLQ2)
+	nom_unc = []
+	nom_unc.append(nom_vals.getValV())
+	nom_unc.append(nom_vals.getErrorLo())
+	nom_unc.append(nom_vals.getErrorHi())
+	nom_unc = UncertaintiesFromTriplet(nom_unc)
+	
 
-    _file1 = TFile.Open(sys_f)
-    fit_mdf = _file1.Get("fit_mdf")
-    sys = fit_mdf.floatParsFinal()
-    sys_index_yLQ2 = sys.index("yLQ2")
-    sys_vals = sys.at(sys_index_yLQ2)
-    sys_unc = []
-    sys_unc.append(sys_vals.getValV())
-    sys_unc.append(sys_vals.getErrorLo())
-    sys_unc.append(sys_vals.getErrorHi())
-    new_unc = UncertaintiesFromTriplet(sys_unc)
-    #nom_unc = getTripletFromFile(nom_f)
-    #new_unc = getTripletFromFile(sys_f)
-    sys_unc = quadDiffAvg(nom_unc, new_unc)
-    return sys_unc
+	_file1 = TFile.Open(sys_f)
+	fit_mdf = _file1.Get("fit_mdf")
+	sys = fit_mdf.floatParsFinal()
+	sys_index_yLQ2 = sys.index("yLQ2")
+	sys_vals = sys.at(sys_index_yLQ2)
+	sys_unc = []
+	sys_unc.append(sys_vals.getValV())
+	sys_unc.append(sys_vals.getErrorLo())
+	sys_unc.append(sys_vals.getErrorHi())
+	new_unc = UncertaintiesFromTriplet(sys_unc)
+	#nom_unc = getTripletFromFile(nom_f)
+	#new_unc = getTripletFromFile(sys_f)
+	sys_unc = quadDiffAvg(nom_unc, new_unc)
+	return sys_unc
 
 ##print diff
 #fileIn = sys.argv[4]
