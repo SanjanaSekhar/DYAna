dcd46a581651329b66bcd81116bb7315701cdd02
Sanjana <sanjanasekhar39@gmail.com>
2024-03-06 14:54:43 -0600

    updating ratio range + ndiv

--- analyze/combine/AFB_fits/scripts/LQ_plot_postfit.py
+++ analyze/combine/AFB_fits/scripts/LQ_plot_postfit.py
@@ -386,8 +386,8 @@ def makeCan(name, tag, histlist, bkglist=[],signals=[],totlist = [], colors=[],t
 						ratio_range = (0.45, 1.55)
 						NDiv = 203
 					elif(mbin ==6 or mbin == 7):
-						ratio_range = (0.0, 2.5)
-						NDiv = 350
+						ratio_range = (0.0, 3.)
+						NDiv = 303
 
 
 
