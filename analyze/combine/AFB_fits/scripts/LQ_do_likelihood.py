dd10d73b275569ccba7ba49f5b5a6b6f80686b7c
Sanjana <sanjanasekhar39@gmail.com>
2024-04-03 20:33:36 -0500

    updated range of likelihoods

--- analyze/combine/AFB_fits/scripts/LQ_do_likelihood.py
+++ analyze/combine/AFB_fits/scripts/LQ_do_likelihood.py
@@ -88,7 +88,7 @@ if options.plot:
         respull = np.asarray(respull, dtype=float)
         poi_list = respull[:,0].tolist()
         deltaNLL = respull[:,1].tolist()
-        plt.xlim(-1,1)
+        #plt.xlim(-1,1)
         plt.ylim(0,5)          
         plt.plot(poi_list,deltaNLL,label='mLQ=%s GeV'%mLQ)
         plt.plot(poi_list,len(poi_list)*[1],linestyle='dashed')
