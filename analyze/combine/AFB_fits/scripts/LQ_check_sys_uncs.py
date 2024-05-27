5d3c7f9395b3b80f5ff87360f84316797705629a
Sanjana <sanjanasekhar39@gmail.com>
2024-05-27 15:35:07 -0500

    updated sys per mass plots

--- analyze/combine/AFB_fits/scripts/LQ_check_sys_uncs.py
+++ analyze/combine/AFB_fits/scripts/LQ_check_sys_uncs.py
@@ -46,6 +46,7 @@ if options.hadd:
 	for is_vec in [True]:
 		for chan in ["mumu","ee"]:
 			for q in ["u","d"]:
+				plt.figure(figsize=(10,7))
 				for options.mLQ in [1000,1500,2000,2500,3000,3500]:
 					for i in range(1,options.nToys+1):
 						#print_and_do("xrdcp -f root://cmseos.fnal.gov//store/user/ssekhar/Condor_outputs/sys_%s_%s%s_%s_exp_toy%i/%s_%s_m%s_sys_uncs_%s_toy1.txt sys_uncs/%s_%s_m%s_sys_uncs_%s_toy%i.txt"%(chan, q, ("_vec" if is_vec else ""),options.mLQ,(i+5), chan, q, options.mLQ, ending,  chan, q, options.mLQ, ending, (i+5) ))
@@ -69,7 +70,6 @@ if options.hadd:
 					df_all.to_csv("sys_uncs/%s_%s_m%s_sys_uncs_%s_alltoys.txt"%(chan, q, options.mLQ, ending),sep=' ',index=False)
 					plt.scatter(df_all['Sys name'],df_all['Mean'], label="mLQ=%i GeV"%(options.mLQ))
 				plt.legend()
-				plt.figure(figsize=(10,7))
 				plt.title("%% contribution of systematics to %s-%s%s channel"%(chan,q,("-vec" if is_vec else "")))
 				plt.xlabel("Systematic")
 				plt.ylabel("%% contribution averaged over 15 toys")
