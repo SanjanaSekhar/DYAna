8c26ca334393c754bab8b428f2dd394f302fa7fa
Sanjana <sanjanasekhar39@gmail.com>
2024-05-29 15:22:14 -0500

    adjusting CMS logo, y range, legend

--- analyze/combine/AFB_fits/scripts/LQ_get_limits.py
+++ analyze/combine/AFB_fits/scripts/LQ_get_limits.py
@@ -40,12 +40,12 @@ def plotLimits(channel):
  # Create an empty TH1 from the first TGraph to serve as the pad axis and frame
     axis = CreateAxisHist(graphs.values()[0])
     #line_sp = ROOT.TLine(1000,1,1755,1)
-    line_sp2 = ROOT.TLine(1755,1.,1755,4)
+    line_sp2 = ROOT.TLine(1755,1.,1755,3.1)
     x = array('d',[860,1175,1355,1755])
     y = array('d',[0.4,0.6,0.8,1.0])
     line_sp = ROOT.TGraph(4,x,y)
 
-    line_pp = ROOT.TLine(1435,0.,1435,4)
+    line_pp = ROOT.TLine(1435,0.,1435,3.1)
     if is_vec:
 	if 'm' in channel: 
 		axis.GetXaxis().SetTitle('m_{V_{#mu %s}} (GeV)'%(channel[0]))
@@ -65,7 +65,7 @@ def plotLimits(channel):
     axis.Draw('axis')
      
      # Create a legend in the top left
-    legend = PositionedLegend(0.75, 0.2, 3, 0.015)
+    legend = PositionedLegend(0.25, 0.2, 3, 0.015)
      
      # Set the standard green and yellow colors and draw
     StyleLimitBand(graphs)
@@ -84,8 +84,8 @@ def plotLimits(channel):
 	line_sp.Draw("same")
 	line_sp2.Draw("same")
 	legend.SetTextSize(0.03)
-	legend.AddEntry(line_pp,"arXiv:1811.01197","L")
-    	legend.AddEntry(line_sp,"arXiv:1509.03750","L")
+	legend.AddEntry(line_sp,"Ref.[19]","L")
+    	legend.AddEntry(line_pp,"Ref.[20]","L")
     legend.Draw()
      
      # Re-draw the frame and tick marks
