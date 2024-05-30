992fe0e27cd832f397ea1a72010b00069144f3f4
Sanjana <sanjanasekhar39@gmail.com>
2024-05-30 01:32:04 -0500

    postfits updated per ccle comments

--- analyze/combine/AFB_fits/scripts/LQ_do_fits.py
+++ analyze/combine/AFB_fits/scripts/LQ_do_fits.py
@@ -25,12 +25,12 @@ parser.add_option("--gen_level",  default=False, action="store_true", help="gen 
 
 #for y in [2016,2017,2018]:
 for y in [-1]:
-    #for options.chan in ["mumu","ee"]:
-    for options.chan in ["ee"]:
-        for options.q in ["u"]:
+    for options.chan in ["mumu","ee"]:
+    #for options.chan in ["ee"]:
+        for options.q in ["u","d"]:
             #mLQ_list = [500,1000,2000,3000]
             mLQ_list = [2500]
-	    is_vec = False
+	    is_vec = True
 	    statuncs = False
 	    #options.gen_level = False
             extra_params=""
