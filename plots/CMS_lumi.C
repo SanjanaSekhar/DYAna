360935fe5903735021799956060893d2afb3bd83
Sanjana <sanjanasekhar39@gmail.com>
2024-07-10 17:34:20 -0500

    checking preliminary

--- plots/CMS_lumi.C
+++ plots/CMS_lumi.C
@@ -158,7 +158,7 @@ CMS_lumi( TPad* pad, int iPeriod, int iPosX )
           latex.SetTextFont(extraTextFont);
           latex.SetTextAlign(align_);
           latex.SetTextSize(extraTextSize*t);
-          latex.DrawLatex(posX_-0.03, posY_+0.09+ relExtraDY*cmsTextSize*t, extraText);
+          latex.DrawLatex(posX_-0.03, posY_+0.09+cmsTextSize*t, extraText);
         }
     }
     }
