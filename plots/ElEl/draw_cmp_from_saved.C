3a7b6da6bbd4d5986213b771ff64dc528bfea485
Sanjana <sanjanasekhar39@gmail.com>
2024-07-19 00:31:06 -0500

    updating ymax for elel

--- plots/ElEl/draw_cmp_from_saved.C
+++ plots/ElEl/draw_cmp_from_saved.C
@@ -417,7 +417,7 @@ void draw_cmp_from_saved(){
 
 
     int year = -1;
-    float hmax = 1500;
+    float hmax = 2000;
     //if(year == 2016)
     //    hmax *= 0.625;
     //if(year == 2017)
@@ -451,7 +451,7 @@ void draw_cmp_from_saved(){
     logy = false;
     int n_cost_bins = 10;
     float cost_bin_size = 2./n_cost_bins;
-    hmax = 3500;
+    hmax = 3000;
 
     //if(year == 2016)
     //    hmax *= 0.625;
