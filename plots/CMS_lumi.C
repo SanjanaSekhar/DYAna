e3810c004f00e4bf80c99711b129f51e5ef8710e
Sanjana <sanjanasekhar39@gmail.com>
2024-09-13 22:33:24 -0500

    modifying the CMS posn

--- plots/CMS_lumi.C
+++ plots/CMS_lumi.C
@@ -152,7 +152,7 @@ CMS_lumi( TPad* pad, int iPeriod, int iPosX )
       latex.SetTextFont(cmsTextFont);
       latex.SetTextSize(cmsTextSize*t);
       latex.SetTextAlign(align_);
-      if(draw_CMS) latex.DrawLatex(posX_-0.03, posY_+0.09, cmsText);
+      if(draw_CMS) latex.DrawLatex(posX_-0.03, posY_-0.2, cmsText);
       
         
 	  
