fb8c4fb40f53387f152c7f1fa4ad913550748b4e
Sanjana <sanjanasekhar39@gmail.com>
2024-10-15 16:46:00 -0500

    postfit tables for hepdata

--- hepdata/save_postfit_tables.py
+++ hepdata/save_postfit_tables.py
@@ -13,17 +13,18 @@ c = [-1, -0.75, -0.5, -0.25, 0, 0.25, 0.5, 0.75, 1]
 for i in range(len(m)-1):
     for j in range(len(y)-1):
         for k in range(len(c)-1):
-            m_str.append("%i-%i" % (i, i+1))
-            y_str.append("%.1f-%0.1f" % (j, j+1))
-            c_str.append("%.2f-%.2f" % (k, k+1))
+	    if y[j] >= 0.6 and (c[k] == -0.75 or c[k] == 0.75): continue
+            m_str.append("%i-%i" % (m[i], m[i+1]))
+            y_str.append("%.1f-%0.1f" % (y[j], y[j+1]))
+            c_str.append("%.2f-%.2f" % (c[k],c[k+1]))
 
-vec = False
+vec = True
 for chan in ["ee","mumu"]:
     for q in ["u","d"]:
-        f_in = TFile.Open("analyze/combine/AFB_fits/postfit_plots/%s_%s%s_unblinded_LQ_m2500/%s_%s%s_unblinded_LQ_m2500.root"%(chan,q,"_vec" if vec else "",chan,q,"_vec" if vec else ""))
+        f_in = TFile.Open("analyze/combine/AFB_fits/postfit_plots/%s_%s%s_unblinded_LQ_m2500/%s_%s%s_unblinded_fit_shapes_LQ.root"%(chan,q,"_vec" if vec else "",chan,q,"_vec" if vec else ""))
         for year in [16,17,18]:
             
-            dir_ = "Y%i_postfit"%year
+            dir_ = "Y%i_postfit/"%year
             print ("\n dir_ = ", dir_)
             
             h_totx = f_in.Get(dir_ + "TotalBkg")
@@ -67,7 +68,7 @@ for chan in ["ee","mumu"]:
             'Observed data': data,
             'Observed data err': data_err
         })
-
+	df = df[['mass (GeV)','rapidity','cos(theta_R)','Background','Background err','LQ template yield','LQ template yield err','Observed data','Observed data err']]
         print(df)
         df.to_csv("hepdata/%s_%s%s_postfit_table.txt" % (chan, q, "_vec" if vec else ""),sep=' ',index=False)
                 
