fa6f4bcc4a4496b4ed6b0134e6e96f13bb509a82
Sanjana <sanjanasekhar39@gmail.com>
2024-12-02 23:39:17 -0600

    changing empty space in plots?

--- analyze/combine/AFB_fits/scripts/LQ_plot_postfit.py
+++ analyze/combine/AFB_fits/scripts/LQ_plot_postfit.py
@@ -52,7 +52,7 @@ def makeCan(name, tag, histlist, bkglist=[],signals=[],totlist = [], colors=[],t
 
 
 	if len(histlist) == 1:
-		width = 32000
+		width = 2900
 		height = 3000
 		padx = 1
 		pady = 1
